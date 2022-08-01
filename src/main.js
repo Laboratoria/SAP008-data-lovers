cb22bb3b2ebb5d569d99115d84fb87aefea729d7
Vanessa Buecker <v.buecker@rocketmail.com>
2022-08-01 16:22:50 -0300

    Faz melhorias no layout e corrige bug de vírgula no map.

--- src/main.js
+++ src/main.js
@@ -14,21 +14,22 @@ function getPokeList() {
     itemType.push(item.type)
     itemRarity.push(item.rarity)
 
-    return ` <div class="container_item">
-        <p class="pokeNumber">${item.num}</p>
-        <div class="boxImg">
-          <figure class= "pokeData">
+    return `<div class="container_item">
+             <div class="boxImg">
+            <p class="pokeNumber">${item.num}</p>
+            <figure class= "pokeData">
             <img class="poke-img" src='${item.img}' alt=${item.name}>
-          </figure>
-          <div class="items">
+            </figure>
+          <div class="items"></div>
           <h4>  ${item.name[0].toUpperCase() + item.name.substr(1)}</h4>
-          <span class="listName listType">Tipo: ${item.type}</span>
-          <span class="listName listRarity">Raridade: ${item["pokemon-rarity"]}</span>
+          <span class="listName"> Tipo: ${item.type}</span>
+          <span class="listRarity"> Raridade: ${item["pokemon-rarity"]}</span>
         </div>
       </div>
       </div>
       `
   })
+  .join('')
 }
 
 getPokeList()
