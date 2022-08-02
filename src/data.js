290f63d070ab62a0aaa9c700d0bbe8657bfe758f
Ellen Cavalcante <srtnelly@hotmail.com>
2022-08-02 14:04:16 -0300

    Funcionalidade ao botao limpar e inclusão dos atributos nname ao campo select e option

--- src/data.js
+++ src/data.js
@@ -1,2 +1,17 @@
+//export const filterByType = (data, selectFilter) =>
+ // data.filter((pokemon) => pokemon.type.includes(selectFilter))
 
 
+
+//export const select = document.getElementById("tipo");
+//const valor = select.value;
+//console.log(valor);
+
+
+/
+
+//function clickTipos() {
+//let tipos = document.getElementById("tipo").value   
+//}
+
+//document.getElementById("tipo").addEventListener("click", clickTipos);
