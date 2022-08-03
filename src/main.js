85620d54ac7df9a5012a7690d64a8a69e7d4a410
marinamassaneiro <marinamassaneiro@gmail.com>
2022-08-03 09:18:42 -0300

    teste soma objeto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { ghibli } from './data.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+//console.log(data);
+
+//teste pra puxar imagem e outras coisas do banco e colocar no html (precisa colocar a id no html)
+/*const poster = document.querySelector("#img-castle-sky");
+const title = document.querySelector("#name-castle-sky")
+const posterUm = data.films[0].poster;
+const titleUm = data.films[0].title;
+poster.innerHTML = "<img src='posterUm'>";
+title.innerHTML = titleUm;
+console.log (posterUm);*/
+
+//teste pra selecionar todos os filmes (ou outros elementos) ta dando um problema quando faz muito grande
+/*for (let i = 0; i < data.films.length; i++) {
+  let gender = data.films[i].people[i].gender;
+  if (gender == "Female"){
+  console.log (gender);
+  }
+}*/
+
+for (let i = 0; i < data.films.length; i++) {
+  let people = data.films[i].people;
+  let sum = people.length;
+  console.log (data.films[i].title + ' tem ' + sum + ' personagens.');
+  } 
+
+
+
