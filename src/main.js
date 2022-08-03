6b7f0ccbee31a8538224b1abf09c0f5b5a32a207
Karla Oliveira <bianca@violet>
2022-08-03 17:28:10 -0300

    tentando acessar o objeto

--- src/main.js
+++ src/main.js
@@ -16,3 +16,29 @@ const ghibli = data.films
 const films = Object.values(ghibli)
 
 films.forEach((film) => console.log(film))
+
+// console.log(films[0].people)
+
+// films.forEach((film) => console.log(film)) // exibe todos os films
+
+const countUnique = (arr) => {
+    return new Set(arr).size;
+}
+
+console.log(countUnique(films[0].people)) //contagem dos people/locations/vehicles por filme
+
+// console.log(countUnique(films[0].people[0].gender))
+
+var genderResult = {}
+
+for(let {gender, name} of films[0].people)
+  genderResult[name] = {
+      name,
+      gender,
+      count: genderResult[name] ? genderResult[name].count + 1 : 1
+  }      
+
+let result = Object.values(genderResult)
+
+console.log(result)
+
