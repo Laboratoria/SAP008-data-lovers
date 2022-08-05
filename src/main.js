843da941503d6b0e2acf9004d17e423db4eae57a
Vanessa Buecker <v.buecker@rocketmail.com>
2022-08-05 12:20:14 -0300

    Cria lógica para o select

--- src/main.js
+++ src/main.js
@@ -1,78 +1,42 @@
 import data from "./data/pokemon/pokemon.js";
 
-//import {BuscarPoke} from "./data.js";
-
+const selectType = document.getElementById("pokeType");
+const searchInput = document.querySelector("#searchPokemon")
+const buttonSearch = document.querySelector('#btn-find')
 const result = document.querySelector(".result")
-let pokemons = filtrarTipo()
-let itemType = [];
-let itemRarity = [];
+const buttonClean = document.getElementById("btn-clean");
+const pokeArray = data.pokemon
 
-function getPokeList() {
-  result.innerHTML = data.pokemon.map((item) => {
-    itemType = [];
-    itemRarity = [];
+buttonSearch.addEventListener("click", filterPokemons)
 
-    itemType.push(item.type)
-    itemRarity.push(item.rarity)
+function printCards(pokeArray) {
 
-    return `<div class="container_item">
-             <div class="boxImg">
-            <p class="pokeNumber">${item.num}</p>
-            <figure class= "pokeData">
-            <img class="poke-img" src='${item.img}' alt=${item.name}>
-            </figure>
-          <div class="items"></div>
-          <h4>  ${item.name[0].toUpperCase() + item.name.substr(1)}</h4>
-          <span class="listName"> Tipo: ${item.type}</span>
-          <span class="listRarity"> Raridade: ${item["pokemon-rarity"]}</span>
-        </div>
-      </div>
-      </div>
-      `
-  })
-  .join('')
-}
-
-getPokeList()
-
-/*
-function todosPoke () {
-const pokeTipo = document.getElementById("result")
-let pokemons = filtrarTipo()
-for (let i = 0; i < pokemons.length; i++) {
-    pokeTipo.innerHTML += `<div class="container_item">
-    <div class="boxImg">
-   <p class="pokeNumber">${pokemons[i].num}</p>
-   <figure class= "pokeData">
-   <img class="poke-img" src='${pokemons[i].img}' alt=${pokemons[i].name}>
-   </figure>
- <div class="items"></div>
- <h4>  ${pokemons[i].name[0].toUpperCase() + pokemons[i].name.substr(1)}</h4>
- <span class="listName"> Tipo: ${pokemons[i].type}</span>
- <span class="listRarity"> Raridade: ${pokemons[i]["pokemon-rarity"]}</span>
-</div>
-</div>
+  result.innerHTML = pokeArray.map(
+    (item) => `<div class="container_item">
+  <div class="boxImg">
+    <p class="pokeNumber">${item.num}</p>
+ <figure class= "pokeData">
+   <img class="poke-img" src='${item.img}' alt=${item.name}>
+ </figure>
+   <div class="items"></div>
+    <h4>  ${item.name[0].toUpperCase() + item.name.substr(1)}</h4>
+<span class="listName"> Tipo: ${item.type}</span>
+<span class="listRarity"> Raridade: ${item["pokemon-rarity"]}</span>
+  </div>
 </div>
 `
-        }
-    } 
-    */
-
- function filtrarTipo () {
-    let teste = data.pokemon.filter((pokemon) => {
-        
-        return pokemon.type.includes("ground")
-        }); 
-        console.log(teste)
-        return (teste)
- }   
-
- getPokeList();
-
-const buttonClean = document.getElementById("btn-clean");
-function cleanFilters() {
-    window.location.reload();
+  ).join('')
 }
-buttonClean.addEventListener("click", cleanFilters);
+printCards(pokeArray)
 
+function filterPokemons() {
+  const select = selectType.value
+  const filterAddPokemons = data.pokemon.filter(({ type }) => type == select)
+  printCards(filterAddPokemons)
+}
+
+buttonClean.addEventListener("click", cleanFilters);
+function cleanFilters() {
+  window.location.reload();
+}
 
