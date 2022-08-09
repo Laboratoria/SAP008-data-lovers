cca0fbe07f867e604c25c29ee9426db9cf8a8de1
marinamassaneiro <marinamassaneiro@gmail.com>
2022-08-09 19:32:30 -0300

    filtro de duracao e pequenos ajustes do CSS responsivo

--- src/main.js
+++ src/main.js
@@ -4,12 +4,9 @@ import data from './data/ghibli/ghibli.js';
 
 const films = data.films;
 const catalogue = document.querySelector('.catalogue');
-// console.log(catalogue);
-// catalogue.innerHTML = "<p>Olá</p>";
 
 function addTela (filmsList) {
- 
-  const filme = filmsList.map(function (film) {
+  const arrayFilms = filmsList.map(function (film) {
     const template = `
     <div class="poster">
       <img class="poster-img" src="${film.poster}" alt="${film.title}" name="${film.title}" >
@@ -20,25 +17,45 @@ function addTela (filmsList) {
     return template; 
   }); 
   // console.log (filme);
-  catalogue.innerHTML = filme.join("");
-
+  catalogue.innerHTML = arrayFilms.join("");
 }
 addTela (films);
 
+const filterDuration = document.querySelector('#filter-duration');
+filterDuration.addEventListener ('change', selectedDuration);
 
-let selectedGender = document.getElementById('filter-gender');
-selectedGender.addEventListener('change', genderFilter);
-
-function genderFilter(evento) { 
-  // console.log (evento.target.value);
-
-  const filmesfiltrados = films.filter(function (film){
-    const personAndGender = film.people.filter (function (person){
-      return person.gender === evento.target.value;
-    }) 
-    // console.log (personAndGender.length, film.people);
-    return personAndGender.length >= (film.people.length/2);
+function selectedDuration(selected) {
+  const filmsDuration = films.filter(function (film){
+    if (selected.target.value === '90min'){
+      return film.duration <= 90;
+    } else if (selected.target.value === '120min'){
+      return film.duration > 90 && film.duration <= 120;
+    } else if (selected.target.value === '150min'){
+      return film.duration > 120;
+    }
+    return film;
   })
-  // console.log (filmesfiltrados);
-  addTela (filmesfiltrados);
+  addTela (filmsDuration)
 }
+
+
+// ======== FILTRO DE GENERO PARA USAR NOS PERSONAGENS =========
+// let selectedGender = document.getElementById('filter-gender');
+// selectedGender.addEventListener('change', genderFilter);
+
+// function genderFilter(event) { 
+//   // console.log (evento.target.value);
+
+//   const filmesfiltrados = films.filter(function (film){
+//     const personAndGender = film.people.filter (function (person){
+//       return person.gender === event.target.value;
+//     }) 
+//     if (event.target.value === 'Gender'){
+//       return film;
+//     }
+//     // console.log (personAndGender.length, film.people);
+//     return personAndGender.length >= (film.people.length/2);
+//   })
+//   // console.log (filmesfiltrados);
+//   addTela (filmesfiltrados);
+// }
