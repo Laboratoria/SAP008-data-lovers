6eb016b8dc7a872d9231c50bc53ebbe78085c121
dodojoy <joyce.mbt@gmail.com>
2022-08-09 21:28:13 -0300

    dando responsividade, criando nav hamburgue, criando modal

--- src/main.js
+++ src/main.js
@@ -3,4 +3,21 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+function changeNavbar(x) {
+    x.classList.toggle('change');
+}
+
+changeNavbar()
+
+function acao() {
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'block';
+}
+acao()
+
+function voltar() {
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'none';
+}
+voltar()
 console.log(example, data);
