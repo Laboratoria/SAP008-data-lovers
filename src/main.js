ba30f5ff4858b94649fdfc3573cd1dfde724b8af
dodojoy <joyce.mbt@gmail.com>
2022-08-11 09:56:33 -0300

    função para darfuncionalidade ao button sobre

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+const btnSobre= document.querySelector('#btn-sobre')
+btnSobre.addEventListener("click", function(){
+    const modal=document.querySelector("#modal");
+    modal.style.display = "flex";
+    const main = document.querySelector("#main-content");
+    main.style.display = "none";
+})
+
+
+
 console.log(example, data);
