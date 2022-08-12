e6e5b033138c5924b612739bd2b6791c6c88e8c3
nunesisabela <isabelanunes.tech@gmail.com>
2022-08-11 21:40:37 -0300

    HTML 1a história do usuário/ CSS com bg img, alguns posicionamentos web (verificar responsividade) e fonte K2D instalada/ main.js com variáveis declaradas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/pokemon/rickandmorty.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+
+const keyWordsTextArea = document.querySelector('key-words')
+const buttonFilter = document.querySelector('filter')
+const buttonSearch = document.querySelector('search')
+
+const radioAlive = document.querySelector('alive')
+const radioDead = document.querySelector('dead')
+
+buttonFilter.addEventListener ('onclick', () => {
+    console.log()
+})
 
 console.log(example, data);
