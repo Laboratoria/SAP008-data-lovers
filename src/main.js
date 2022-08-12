673aa76a1afc039d647d8eea2b08f16df30b29c8
blericalopes <blericalopes@outlook.com>
2022-08-12 13:01:49 -0300

    funcionalidade do botão filtrar e section filter box escondida

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/rickandmorty.js';
+//import data from './data/pokemon/rickandmorty.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-const keyWordsTextArea = document.querySelector('key-words')
-const buttonFilter = document.querySelector('filter')
-const buttonSearch = document.querySelector('search')
+//const keyWordsTextArea = document.querySelector('key-words')
+const buttonFilter = document.querySelector('#filter');
+//const buttonSearch = document.querySelector('search')
 
-const radioAlive = document.querySelector('alive')
-const radioDead = document.querySelector('dead')
+//const radioAlive = document.querySelector('alive')
+//const radioDead = document.querySelector('dead')
 
-buttonFilter.addEventListener ('onclick', () => {
-    console.log()
+
+const filterBox = document.querySelector('#filter-box');
+filterBox.hidden = true;
+buttonFilter.addEventListener ('click', () => {
+    filterBox.hidden = false;
+
+    
 })
 
-console.log(example, data);
+//console.log(example, data);
+
+
