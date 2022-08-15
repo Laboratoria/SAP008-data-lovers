9e0d6d0c183f24ceb809ff0633b8219e3e46e470
dodojoy <joyce.mbt@gmail.com>
2022-08-15 14:07:09 -0300

    printando os cards e suas informações na tela

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/lol/lol.js';
+
+let arrayCampeoes = Object.values(data.data);
 
 const btnSobre = document.querySelector('#btn-sobre');
 const main = document.querySelector("#main-content");
-const modal=document.querySelector("#modal");
+const modal = document.querySelector("#modal");
+const cards = document.querySelector('#cards');
 btnSobre.addEventListener('click', function(){
     modal.style.display = 'flex';
     main.style.display = 'none';
@@ -23,6 +23,31 @@ voltarFooter.addEventListener('click', function(){
     main.style.display = 'none';
 })
 
+const btnIniciar = document.querySelector('#iniciar');
+btnIniciar.addEventListener('click', function(){
+    main.style.display = 'none';
+    cards.style.display = 'flex';
+})
 
 
-console.log(example, data);
+function printCards(lol) {
+    document.getElementById('cardzinho').innerHTML = lol.map((key) => 
+        `
+            <div class="card-container">
+                <div class="card-front">
+                    <img src="${key.splash}">
+                    <h2>${key.name}</h2>
+                </div>
+                <div class="card-hover">
+                    <ul>
+                        <li>ATAQUE: ${key.info.attack}</li>
+                        <li>DEFESA: ${key.info.defense}</li>
+                        <li>MAGIA: ${key.info.magic}</li>
+                        <li>DIFICULDADE: ${key.info.difficulty}</li>
+                    </ul>
+                    <h2>${key.name}</h2>
+            </div>
+        `    
+    )
+}
+printCards(arrayCampeoes)
