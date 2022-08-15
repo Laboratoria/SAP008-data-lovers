3c5598a75a0f759ed860fb1362b35c1589b00bcc
Clarissa Simplício <clasimplicio@hotmail.com>
2022-08-15 14:09:24 -0300

    funções de cálculo, ordenação e filtros por tipo e geração

--- src/data.js
+++ src/data.js
@@ -1,30 +1,25 @@
 
-// estas funciones son de ejemplo
-
-export const filterType = (data, condition) =>{
-  return data.filter((key) => key.type.includes(condition))
+export const filterType = (data, requirement) =>{
+  return data.filter((key) => key.type.includes(requirement))
 }
 
-export const filterGeneration = (data, condition) =>{
-  return data.filter((key) => key.generation.name.includes(condition))
+export const filterGeneration = (data, requirement) =>{
+  return data.filter((key) => key.generation.name.includes(requirement))
 }
 
-/* export const sortData = (data, sortBy) =>{
-  if(sortBy=="A-Z"){
-    return data.sort();
-  }else{
-    return data.sort().reverse();
+export const sortData = (data) =>{
+  return data.sort((a, b) => {
+    if (a.name > b.name) {
+      return 1
+    }
+    if (a.name < b.name) {
+      return -1
+    }
+    return 0
+    }
+  )
+};
 
-  }
-} */
-
-
-  /* export const example = (data) => {
-    console.log(data)
-    return 'example';
-  };
-  
-  export const anotherExample = () => {
-    return 'OMG';
-  }; */
-
+export const calCp = (data,position,cp)=> {
+  return data[position].stats['max-cp'] - cp
+}
