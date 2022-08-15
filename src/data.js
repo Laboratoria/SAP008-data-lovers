3a28852e211cc1a22e21e5121e663df7711ae58e
AngelMelo12 <angellmello1993@gmail.com>
2022-08-14 23:11:59 -0300

    Adição de evento nos botões de filtragem, e ciação de função de filtragem

--- src/data.js
+++ src/data.js
@@ -1,23 +1,19 @@
-/*import data from "./data/rickandmorty/rickandmorty.js";
-
-export consts = {displayResults};
+import data from "./data/rickandmorty/rickandmorty.js";
 
 
-function displayResults() {
-
-    let resultado = '';
-    let todosOsDados = data.results;
-
-    for (let i = 0; i < todosOsDados.length; i++) {
-        const resultsList = todosOsDados[i];
-        resultado = resultado + resultsList
-        
-    }
-    
-    return resultado
-    
-}*/
+//export const example = () => {
+ //   return 'example';
+ // };
 
 
+export const filtrarPorEspecie = (species) => {
+    return data.results.filter(personagem => personagem.species.toLowerCase() === species)
+};
 
-
+//Criei uma constante que é uma funcáo, onde passei o parametro que quero que seja filtrado, que nesse caso é o status
+// essa arrow function indica que aqui dentro vai ter uma cocndicao que vai ser necessaria para filtrar cada elemento dentro da array.
+// obs - .filter também é um loop que percorre todos os elementos do array.
+// pedi para retornar essa condiçao em forma de filtro, onde estou acessando o elemento personagem dentro do array,
+//ai coloquei personagem.status.toLowerCase (coloquei tolowercase, strings minusculas) para facilitar a validacao junto com ()
+//para indicar a função, os === sao para bater o valor identico de tipo e valor.
+//
