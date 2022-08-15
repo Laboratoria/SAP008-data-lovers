01b2f926a03e3c8dd233c298df6828b003e439cb
Bellalimalima <isabellalimaelima@gmail.com>
2022-08-15 19:45:29 -0300

    printamos cards

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import data from './data/rickandmorty/rickandmorty.js';
 // IMPORTANDO TODOS OS DADOS DOS PERSONAGENS: 
 const rickandmorty = data.results;
 
-// CRIANDO OS CARDS NA TELA
+// PRINTANDO OS CARDS NA TELA
 function criaCard (personagens){
 const exibir = personagens.map((personagem) => {
 const template = `
@@ -31,26 +31,12 @@ document.querySelector("#exibirCards").innerHTML = exibir.join("<br>");
 criaCard(rickandmorty);
 
 
-//PRINTAR TODOS OS CARDS NA TELA:
-//const exibir = rickandmorty.map(criaCard).join("");
 
 
 
-    //return personagem.name + " " + personagem.species + " " + personagem.gender + " " + personagem.status + "<br>";
-        `<div class = "cards">
-            <div class = "imagem">
-             <img id = "imagemCard" src= "${personagem.image}"></img>
-             <div class = "name">${personagem.name}</div>
-            </div>
-            <ul class = "textoCard">
-             <li>Espécie: ${personagem.species}</li>
-             <li>Gênero: ${personagem.gender}</li>
-             <li>Status: ${personagem.status}</li>
-            </ul>
-        </div>
-        `
 
 
+      
 
     
     
