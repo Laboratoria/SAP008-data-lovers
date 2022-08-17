95aee6c95087854342e6d19096fd636bcaba7184
AngelMelo12 <angellmello1993@gmail.com>
2022-08-17 11:29:14 -0300

    Ajustes das filtragens

--- src/data.js
+++ src/data.js
@@ -1,12 +1,13 @@
-export {rickAndMorty};
+ 
 
+export {rickAndMorty};
 // ==== objeto principal com as funções de mostrar cards e filtro =====
 const rickAndMorty = {
 
     // ======= Função de gerar templates ======
     displayCards:
-    function displayCards(cardsList) {
-        const arrayResults = cardsList.map((item) => {
+    function displayCards(list) {
+        const arrayResults = list.map((item) => {
             const template = `
             <div class="card">
     
@@ -25,41 +26,51 @@ const rickAndMorty = {
             `;
             return template;
         });
-        return arrayResults.join("");
+        return arrayResults.join(""); //retorno do resultado da função depois de mapeada e montado o template.
     },  
 
     // ===== Função de filtro por status ====
     filtrarPorStatus:    
-    function filtrarPorStatus(status) {
-    const filtro = data.results.filter(function(personagem) {
-        const filtrado = personagem.status.toLowerCase() === status;
-        return filtrado;
-    });
+    function filtrarPorStatus(list, status) {
+        //Aqui adicionei no parâmetro o data, pq ele recebe uma lista e ele vai retornar essa lista que ele recebeu filtrada.
+        //isso é necessario para fazermos os testes e é o recomendado pelo read.me no src/data.js
+        const filtro = list.filter(function(personagem) {
+        //Aqui coloquei data pois agora a lista está vindo através do main.js.
+        const statusFiltrado = personagem.status.toLowerCase() === status; //Alterei o nome da constante para ficar mais claro de identificarmos
+        return statusFiltrado; 
 
-    return this.displayCards(filtro)
+    });
+        return this.displayCards(filtro) //Aqui no retorno esse this significa que ele (o objeto) está se referindo a
+        //a ele mesmo, e está retornando o template html da lista filtrada.
     },
 
     // ===== Função de filtro por espécie ====
     filtrarPorEspecie:    
-    function filtrarPorEspecie(species) {
-        const filtro = data.results.filter(personagem => personagem.species.toLowerCase() === species);
-        return filtro;   
+    function filtrarPorEspecie(list, species) {
+        const filtro = list.filter(function(personagem) {
+            const especieFiltrado = personagem.species.toLowerCase() === species;
+            return especieFiltrado;   
+        })
+
+        return this.displayCards(filtro)
     },
 
     // ===== Função de filtro por gênero ====
     filtrarPorGenero:    
-    function filtrarPorGenero (gender) {
-        const filtro = data.results.filter(personagem => personagem.gender.toLowerCase() === gender)
-        return filtro
-    }
+    function filtrarPorGenero (list, gender) {
+        const filtro = list.filter(function(personagem) {
+            const generoFiltrado = personagem.gender.toLowerCase() === gender;
+            return generoFiltrado
+        })
+            return this.displayCards(filtro)
 
+    },
 };
-    
+
 
 //Criei uma constante que é uma funcáo, onde passei o parametro que quero que seja filtrado, que nesse caso é o status
 // essa arrow function indica que aqui dentro vai ter uma cocndicao que vai ser necessaria para filtrar cada elemento dentro da array.
 // obs - .filter também é um loop que percorre todos os elementos do array.
 // pedi para retornar essa condiçao em forma de filtro, onde estou acessando o elemento personagem dentro do array,
 //ai coloquei personagem.status.toLowerCase (coloquei tolowercase, strings minusculas) para facilitar a validacao junto com ()
-//para indicar a função, os === sao para bater o valor identico de tipo e valor.
-//
+//para indicar a função, os === sao para bater o valor identico de tipo e valor
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 // ==== constantes e DOM's=====
 const list = data.results;
-const cardContainer = document.querySelector('.card-container');
+const cardContainer = document.querySelector('#card-container');
 const selectStatus = document.querySelector('#select-status');
 const selectSpecies = document.querySelector('#select-species');
 const selectGender = document.querySelector('#select-gender');
@@ -15,31 +15,28 @@ cardContainer.innerHTML = rickAndMorty.displayCards(list);
 // ===== evento para filtrar por status ====
 selectStatus.addEventListener('change', (event) => {
     const value = event.target.value;
-    const listaFiltrada = rickAndMorty.filtrarPorStatus(value);
-    /*document.querySelector('.card-container').innerHTML = rickAndMorty.displayCards(listaFiltrada);*/
-    console.log(listaFiltrada);
+    const listaFiltrada = rickAndMorty.filtrarPorStatus(list, value);
+    //tive que acrescentar o data.results pois o filtrar por status agora recebe 2 parametros,
+    //o data.results para que o filtro acesse os itens do objeto que estão no data e o value
+    //valor da ação do usuário quando escolhe um dos status..
+    document.querySelector('#card-container').innerHTML = listaFiltrada;
+  
       
 });
 
 // ===== evento para filtrar por especie ====
 selectSpecies.addEventListener('change', (event) => {
-     
     const value = event.target.value;
-    const listaFiltrada = rickAndMorty.filtrarPorEspecie(value);
-    cardContainer.innerHTML = rickAndMorty.displayCards(listaFiltrada);
-    return listaFiltrada;
-    //O .value no final, estamos indicando que essa constante tem que receber o valor atribuido ao botão (const value), que sera disparado 
-    //quando o usuario escolher no select qual a espécie. 
-     // Criamos uma constante onde ela recebeu o valor da funçào que está no data. 
-
+    const listaFiltrada = rickAndMorty.filtrarPorEspecie(list, value);
+    document.querySelector('#card-container').innerHTML = listaFiltrada;
+    
 });
 
 // ===== evento para filtrar por genero ====
 selectGender.addEventListener('change', (event) => {
     const value = event.target.value;
-    const listaFiltrada = rickAndMorty.filtrarPorGenero(value);
-    cardContainer.innerHTML = rickAndMorty.displayCards(listaFiltrada);
-    return listaFiltrada;
+    const listaFiltrada = rickAndMorty.filtrarPorGenero(list, value);
+    document.querySelector('#card-container').innerHTML = listaFiltrada;
 
 });
 
@@ -48,6 +45,10 @@ selectOrder.addEventListener('change', (event) => {
     const value = event.target.value;
     window.alert(value);
 });
+
+
+
+
 
 
 /* Queremos que apareça na tela somente os personagens que são da especie humana.
