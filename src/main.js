981f7e56bcb8dccbfa93c2c193be791b1d60c72a
brunalimxst <brunalima2160@hotmail.com>
2022-08-17 16:26:25 -0400

    teste

--- src/main.js
+++ src/main.js
@@ -4,34 +4,35 @@ import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 function cardsData(data) {
+  
   return data.map((item) => `
 
-  <div class="card">
-    <div class="card-in"> 
-      <div class="card-first">
-      <img src="${item.image}">
-       <p><strong> Nome: ${item.name}</strong></p>
-    </div> 
-    
-    <div class="back-card">
-      <ul class="list-card">
-        <li>Nome: ${item.name}</li>
-        <li>Status: ${item.status}</li>
-        <li>Espécie: ${item.species}</li>
-        <li>Gênero: ${item.gender}</li>      
-        <li>Localização: ${item.location.name}</li>
-      </ul>
+    <div class="card">
+      <div class="card-in"> 
+        <div class="card-first">
+        <img src="${item.image}">
+         <p><strong> Nome: ${item.name}</strong></p>
+      </div> 
+      
+      <div class="back-card">
+        <ul class="list-card">
+          <li>Nome: ${item.name}</li>
+          <li>Status: ${item.status}</li>
+          <li>Espécie: ${item.species}</li>
+          <li>Gênero: ${item.gender}</li>      
+          <li>Localização: ${item.location.name}</li>
+        </ul>
+      </div>
     </div>
-  </div>
-</div>
-`)
+  </div>  
+  `)
 
-.join('')
+  .join('')
 }
-document.getElementById('read').innerHTML = cardsData(data.results)
-  
-  const selectGender = document.getElementById("gender");
-  const selectSpecies = document.getElementById("species");
-  const selectStatus = document.getElementById("status"); 
+document.getElementById('read').innerHTML = cardsData(data.results);
+
+const selectGender = document.getElementById("gender");
+const selectSpecies = document.getElementById("species");
+const selectStatus = document.getElementById("status");
 
 
