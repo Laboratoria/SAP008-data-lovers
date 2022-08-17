6a7ae93f35bcceeb8151ef5ce5c45dd14be51de9
Andrea dos Santos <contato.andreadossantos@gmail.com>
2022-08-17 18:10:43 -0300

    Print dos Cards na Tela

--- src/data.js
+++ src/data.js
@@ -1,9 +1,20 @@
-// estas funciones son de ejemplo
+//export const alphabeticOrder = (data) => data.sort((a,z) => a.name > z.name ? 1 : -1);//
 
-export const example = () => {
-  return 'example';
-};
+export const sortByAZ = (data) => alphabeticOrder(data).slice(0, 494);
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const sortByZA = (data) => sortByAZ(data).reverse();
+  
+export const filterData = (data,typeData, condition) => data.filter((character) => {
+   return (character[typeData] === condition);
+})
+
+export const filterName = (data, search) => data.filter(character => {
+  return character.name.toLowerCase().includes(search.toLowerCase());
+})
+
+export const computeStats = (data,typeData, condition) => data.reduce((initialType, totalType) => {
+  return initialType + (totalType[typeData] === condition);
+},0)
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import {sortByAZ, sortByZA, filterData, filterName, computeStats} from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
+
+function showCards(data) {
+  document.getElementById('listening').innerHTML = data.map((item) => `
+  <div class="flip-container">
+     <div class="flipper">
+        <div class="front">
+           <img  class= "image" src="${item.image}">
+           <h3><strong> ${item.name}</strong></h3>
+           <p class="text">${item.species}</p>
+           <p class="text">${item.gender} * ${item.status}</p> 
+           <p class="text">${item.location.name}<p>
+        </div> 
+        <div class="back">
+        <h2>${item.name}</h2>
+        </div>
+    </div>
+</div>  
+`)
+}
+
+showCards(data.results);
+  
