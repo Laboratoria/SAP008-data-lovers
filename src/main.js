defe259f838f33676620f87bf4f0da5904cd730e
brunalimxst <brunalima2160@hotmail.com>
2022-08-17 16:17:45 -0400

    res de conflito

--- src/main.js
+++ src/main.js
@@ -4,29 +4,31 @@ import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 function cardsData(data) {
-    document.getElementById('read').innerHTML = data.map((item) => `
-      <section class="card">
-        <div class="card-face">
-          <div class="card-face-front">
-          <img src="${item.image}">
-          <p><strong>${item.name}</strong></p>
-          </div>
+  return data.map((item) => `
 
-          <div class="card-face-back">
-            <ul class="list-card">
-              <li>Nome: ${item.name}</li>
-              <li>Status: ${item.status}</li>
-              <li>Espécie: ${item.species}</li>
-              <li>Gênero: ${item.gender}</li>      
-              <li>Localização: ${item.location.name}</li>
-            </ul>
-          </div>
-        </div>
-      </section>
-  `)
-  }
-  
-  cardsData(data.results);
+  <div class="card">
+    <div class="card-in"> 
+      <div class="card-first">
+      <img src="${item.image}">
+       <p><strong> Nome: ${item.name}</strong></p>
+    </div> 
+    
+    <div class="back-card">
+      <ul class="list-card">
+        <li>Nome: ${item.name}</li>
+        <li>Status: ${item.status}</li>
+        <li>Espécie: ${item.species}</li>
+        <li>Gênero: ${item.gender}</li>      
+        <li>Localização: ${item.location.name}</li>
+      </ul>
+    </div>
+  </div>
+</div>
+`)
+
+.join('')
+}
+document.getElementById('read').innerHTML = cardsData(data.results)
   
   const selectGender = document.getElementById("gender");
   const selectSpecies = document.getElementById("species");
