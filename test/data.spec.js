42a19dd9750997b624c183a1420422d7d2767b99
Ahda <amandaholanda_@hotmail.com>
2022-08-17 19:57:56 -0300

    testes unitarios

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,13 +1,77 @@
 import {rickAndMorty} from '../src/data.js';
 
-describe('actions', () => {
+const arrayTest = [
+  {
+  "status": "Alive",
+  "species": "Human",
+  "gender": "Male",
+  },
+
+  {
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male",
+  },
+
+  {
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Female",
+  },
+
+  {
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Female",
+  } 
+];
+
+
+
+describe('rickAndMorty', () => {
+
   it('rickAndMorty is a object', () => {
     expect(typeof rickAndMorty).toBe('object');
-  })
-  it('displayCards is a function', () => {
+  });
+});
+  
+  
+/*describe('rickAndMorty.displayCards', () => {
+  it('should be a function', () => {
     expect(typeof rickAndMorty.displayCards).toBe('function');
   });
-  it('filtrarPorStatus is a function', () => {
+
+  it('should return 493 characters', () => {
+    expect(rickAndMorty.displayCards().length).toBe(493)
+  })
+
+});*/
+
+describe('filtrarPorStatus', () => {
+  it('é uma função', () => {
     expect(typeof rickAndMorty.filtrarPorStatus).toBe('function');
-  });  
+
+  });
+
+  it('filtra o status dos personagens', () => {
+    const expectedStatus = rickAndMorty.filtrarPorStatus(arrayTest, "Alive");
+    expect(expectedStatus).toEqual([{
+      "status": "Alive",
+      "species": "Human",
+      "gender": "Male",
+    }]);
+  });
 });
+    
+/*describe('filtrarPorStatus', () => {
+
+    it('should be a function', () => {
+      expect(typeof rickAndMorty.filtrarPorStatus).toBe('function');
+    });
+    
+    it('should return "tal" for "tal" ', () => {
+      expect(rickAndMorty.filtrarPorStatus()).toBe();
+    });
+    
+  });*/
+
