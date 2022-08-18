244b4ac81e12a045fbd0d535d1f431ba9db36130
adevcarol <alvscarolina@outlook.com>
2022-08-18 06:32:07 -0300

    Criação do forEach que irá printar os primeiros cards

--- src/data.js
+++ src/data.js
@@ -1,14 +1,9 @@
 // estas funciones son de ejemplo
 
 export const example = () => {
-  let athletes = example.athletes;
-
-  let atletas = athletes.map(function (item, indice) {
-    return item.name;
-  });
-
-  return atletas;
+  // ...
 };
-/*export const anotherExample = () => {
-  return 'OMG';
-};*/
+
+export const anotherExample = () => {
+  // ...
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { example } from './data.js';
+// import { example } from './data.js';
 import data from './data/athletes/athletes.js';
 
-console.log(example, data);
+// console.log(data);
+
+const content = document.querySelector('.cards-main');
+
+// const data2 = [data.athletes[0], data.athletes[1], data.athletes[2]]
+const arrAtletas = data.athletes
+
+arrAtletas.forEach((element, index) => {
+    if (index <= 8) {
+        const template = `
+            <div class="card-atleta" id="card-atleta">
+                <div class="atributos-atleta">
+                    <p class="nome">${element.name}</p>
+                    <p class="genero">${element.gender}</p>
+                    <p class="esporte">${element.sport}</p>
+                    <p class="pais">${element.team}</p>
+                    <p class="medalha">${element.medal}</p>
+                </div>
+            </div>
+            `
+        content.innerHTML += template
+    }
+    console.log(arrAtletas.length)
+});
