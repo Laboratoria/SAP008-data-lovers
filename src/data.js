9098c98f9480f39ebac222f838600983ff128173
nunesisabela <isabelanunes.tech@gmail.com>
2022-08-18 14:50:59 -0300

    função de filtrar funcionando/ função de mostrar cards funcionando/ estilização fonte cards ok

--- src/data.js
+++ src/data.js
@@ -1,20 +1,26 @@
-// export const example = () => {
-//   return 'example';
-// };
-
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
-
-export function filterData (data) {
-    // var charactersName = data.results.map(elemento => elemento.name)
-    // var charactersStatus = data.results.map(elemento => elemento.status)
-    // var charactersSpecies = data.results.map(elemento => elemento.species)
-
-    
-    // return charactersName, charactersStatus, charactersSpecies
+export function filterData(array, property, value) { //função para filtrar dados
+  return array.results.filter(elem => elem[property] === value) //.filter somente acessa arrays. para acessar objetos, usamos "." (literal, como em .results)
 }
 
-export function filtroAlive (dados) {
-  return 'banana'
+export function showCards(infos) { //função para exibir cards
+  return infos.map((elem) => `
+    <div class="cards">
+    <img id="photos" src="${elem.image}" alt="imagem do personagem"><br>
+    <p id="names">${elem.name}</p><br>
+    
+      <ul class="cards-list">
+      
+        <p>Status: </p>
+        <li>${elem.status}</li>
+
+        <p>Gênero: </p>
+        <li>${elem.gender}</li>
+        
+        <p>Espécie: </p>
+        <li>${elem.species}</li>
+      
+      </ul>
+    </div>
+    `
+  )//.join('')
 }
