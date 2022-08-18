3ae954d2376d73cc7765ef10312e440d5104a989
Polyanagm <polyana.gm@hotmail.com>
2022-08-18 13:59:29 -0300

    funções dos filtros

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 
 function cardsData(data) {
-  return data.map((item) => `
+  document.getElementById('read').innerHTML = data.map((item) => `
 
-    <div class="card">
+    <section class="card">
       <div class="card-in"> 
         <div class="card-first">
         <img src="${item.image}">
@@ -27,15 +27,35 @@ function cardsData(data) {
         </ul>
       </div>
     </div>
-  </div>  
+  </section>  
   `)
-
-  .join('')
 }
-document.getElementById('read').innerHTML = cardsData(data.results);
-
-const selectGender = document.getElementById("gender");
-const selectSpecies = document.getElementById("species");
-const selectStatus = document.getElementById("status");
+cardsData(data.results);
 
 
+const selectGender = document.querySelector(".select-gender");
+const selectSpecie = document.querySelector(".select-specie");
+const selectStatus = document.querySelector(".select-status");
+const teste = document.getElementById("teste");
+
+
+
+function genderFiltro() {
+  teste.style.display = "flex";
+  /*teste.innerHTML = `O número de personagens dessa categoria é ${computeStats(data.results, "gender", selectGender.value)}`*/
+  return cardsData(dataFilter(data.results, "gender", selectGender.value));
+}
+
+function specieFiltro() {
+  teste.style.display = "flex";
+  /*teste.innerHTML = `O número de personagens dessa categoria é ${computeStats(data.results, "species", selectSpecie.value)}`*/
+  return cardsData(dataFilter(data.results, "species", selectSpecie.value));
+}
+function statusFiltro() {
+  teste.style.display = "flex";
+  return cardsData(dataFilter(data.results, "status", selectStatus.value));
+}
+
+selectGender.addEventListener("change", genderFiltro);
+selectSpecie.addEventListener("change", specieFiltro);
+selectStatus.addEventListener("change", statusFiltro);
