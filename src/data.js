6b1e7cb328fcb89fa2e5e9801a567db9e7b042e8
brunabrubs <brunanunessouza5@gmail.com>
2022-08-19 18:32:24 -0300

    commit inicial

--- src/data.js
+++ src/data.js
@@ -1,9 +1,27 @@
-// estas funciones son de ejemplo
+export const pokemonByType = (data, type) => {
+  const filteredType = data.filter(pokemonType => pokemonType.type.find(item => item === type) === type)
+  return filteredType
+}
 
-export const example = () => {
-  return 'example';
-};
+export const pokemonByRarity =(data,rarity) => {
+  const filteredRarity = data.filter(pokemonRarity => pokemonRarity["pokemon-rarity"] == rarity)
+  return filteredRarity
+}
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const pokemonByEggs =(data,egg) =>{
+  const filterEggs = data.filter(pokemonEggs => pokemonEggs.egg == egg)
+  return filterEggs
+}
+
+export const pokemonByName = (data, name) => {
+  const filterName = data.filter(pokemonName => pokemonName.name.includes(name))
+  return filterName
+}
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import {pokemonByType,pokemonByRarity,pokemonByEggs} from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// guarda os dados dos pokemons na let pokemon
+let pokemons = data.pokemon;
+
+function getAllPokemons(data){
+    const pokeInfo = data.map((item) => {
+      return `
+      <div class="cards" id="cards">
+            <img class="image-card" src="${item.img}" alt="imagem do Pokémon" loading = "lazy">
+            <div class="info-cards">
+            
+            </div>
+      </div>
+      `
+    }).join('')
+  
+    document.getElementById('pokemonList').innerHTML = pokeInfo;
+  }
+
+  getAllPokemons(pokemons);
