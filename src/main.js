756afb2cee178f8f91141e6a89d893ad3fedd61e
nunesisabela <isabelanunes.tech@gmail.com>
2022-08-19 13:06:00 -0300

    função showCards de volta para main.js/ check no lugar de radio input no HTML/ checks declarados no main.js por name/ event.prevent para não recarregar pg

--- src/main.js
+++ src/main.js
@@ -1,80 +1,57 @@
 import { filterData, showCards } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
+const filterBox = document.querySelector('#filterbox');
 const buttonFilter = document.querySelector('#filter');
-const filterBox = document.querySelector('#filter-box');
-filterBox.hidden = true;
-buttonFilter.addEventListener('click', () => { //botão FILTRAR (exibe filtros)
-    filterBox.hidden = false;
-});
-
 const buttonSearch = document.querySelector('#search');
 const cards = document.querySelector('#cards');
-const radioButtonAlive = document.querySelector("#alive");
-const radioButtonDead = document.querySelector("#dead");
-const radioButtonFemale = document.querySelector("#female");
-const radioButtonMale = document.querySelector("#male");
-const radioButtonHuman = document.querySelector("#human");
-const radioButtonNonHuman = document.querySelector("#non-human");
+const form = document.querySelector('#form');
 
-buttonSearch.addEventListener('click', () => { //botão BUSCAR (filtra, exibe cards)
-
-    const x = filterData(data, "status", "Alive")
-    console.log(x)
-
-    cards.innerHTML = showCards(data.results)
+filterBox.hidden = true;
+buttonFilter.addEventListener('click', (event) => { //exibe filtros
+    filterBox.hidden = false;
+    event.preventDefault() //adicionar ao buttonSearch.addEventListener também
 });
 
-let checked = true;
-radioButtonAlive.addEventListener('click', () => {
-    if (checked == true) {
-        checked = false
-    } else {
-        checked = true
-    }
-    radioButtonAlive.checked = checked;
-});
-radioButtonDead.addEventListener('click', () => {
-    if (checked == true) {
-        checked = false
-    } else {
-        checked = true
-    }
-    radioButtonDead.checked = checked;
-});
-radioButtonFemale.addEventListener('click', () => {
-    if (checked == true) {
-        checked = false
-    } else {
-        checked = true
-    }
-    radioButtonFemale.checked = checked;
-});
-radioButtonMale.addEventListener('click', () => {
-    if (checked == true) {
-        checked = false
-    } else {
-        checked = true
-    }
-    radioButtonMale.checked = checked;
-});
-radioButtonHuman.addEventListener('click', () => {
-    if (checked == true) {
-        checked = false
-    } else {
-        checked = true
-    }
-    radioButtonHuman.checked = checked;
-});
-radioButtonNonHuman.addEventListener('click', () => {
-    if (checked == true) {
-        checked = false
-    } else {
-        checked = true
-    }
-    radioButtonNonHuman.checked = checked;
+buttonSearch.addEventListener('click', () => { //filtra e exibe cards
+    const keyWords = form.keywords.value //acessa valor do input de texto
+    const statusChecked = document.querySelectorAll('input[name="status"]:checked');
+    const genderChecked = document.querySelectorAll('input[name="gender"]:checked');
+    const speciesChecked = document.querySelectorAll('input[name="species"]:checked');
+
+    const charactersKeyWords = filterData(data, argumeto, keyWords); // ???
+    const charactersStatus = filterData(data, statusChecked, statusChecked.value); // ???
+    const charactersGender = filterData(data, genderChecked, genderChecked.value); // ???
+    const charactersSpecies = filterData(data, speciesChecked, charactersUnknow.value); // ???
+
+    function showCards(infos) { //exibe cards
+        return infos.map((elem) => `
+          <div class="cards">
+          <img id="photos" src="${elem.image}" alt="imagem do personagem"><br>
+          <p id="names">${elem.name}</p><br>
+          
+            <ul class="cards-list">
+            
+              <p>Status: </p>
+              <li>${elem.status}</li>
+      
+              <p>Gênero: </p>
+              <li>${elem.gender}</li>
+              
+              <p>Espécie: </p>
+              <li>${elem.species}</li>
+            
+            </ul>
+          </div>
+          `
+        ).join('');
+    };
+    cards.innerHTML = showCards(charactersAlive)
 });
 
-console.log(filterData, data);
 
-
+// L29 E os personagens não-humanos? Como filtrá-los?
+// L44 Como puxar radio buttons pela class?
+// Como funciona o .join() depois do template?
+// CSS como scrollar apenas os cards na página?
+// Como usar as tipografias escolhidas?
