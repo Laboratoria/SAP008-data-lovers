4aacb3870e612e8c54bd4a4c24e4193c6632c054
marinamassaneiro <marinamassaneiro@gmail.com>
2022-08-19 09:01:58 -0300

    cardpersonagenscss

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
-export const ghibli = {printCatalogue, printModal, filterDuration, sortedFilms, 
-  countFilms, countPeopleModal, countLocations, countPeople, countVehicles};
+export const ghibli = {printCatalogue, printModal, filterDuration, sortedFilms, countFilms, countPeopleModal, 
+  countLocations, countPeople, countVehicles, printCataloguePeople};
 
 function printCatalogue(filmsList) {
   const arrayFilms = filmsList.map((film) => {
     const template = `
-    <div class="card">
-      <button class= "btn-modal" type="button">
-      <img class="card-img" src="${film.poster}" alt="${film.title}" name="${film.title}" > </button>
-      <br><h7>${film.title}</h7><br>
-      <h7>(${film.release_date})</h7>
-    </div>
-    `;
+      <div class="card">
+        <button class= "btn-modal" type="button"> 
+        <img class="card-img" src="${film.poster}" alt="${film.title}" name="${film.title}" > </button>
+        <br><h7>${film.title}</h7><br>
+        <h7>${film.release_date}</h7>
+      </div>
+      `;
     return template; 
   }); 
-  return arrayFilms.join('');
+return arrayFilms.join('');
 }
 
 function printModal (film) {
@@ -39,9 +39,9 @@ function filterDuration (films, selection) {
     if (selection === '90min') {
       return film.duration <= 90;
     } else if (selection === '120min'){
-      return film.duration > 90 && film.duration <= 120;
+        return film.duration > 90 && film.duration <= 120;
     } else if (selection === '150min'){
-      return film.duration > 120;
+        return film.duration > 120;
     }
     return film;
   })
@@ -53,9 +53,9 @@ function sortedFilms(films, selection) {
     if (selection === 'rt_score') {
       return b.rt_score - a.rt_score;
     } else if (selection === 'release_date') {
-      return b.release_date - a.release_date;
+        return b.release_date - a.release_date;
     } else if (selection === 'order') {
-      return a.release_date - b.release_date
+        return a.release_date - b.release_date
     }
   });
   return sorted;
@@ -68,32 +68,83 @@ function countFilms (films) {
 function countPeopleModal (films) {
   const count = films.map((film) => {
     return (film.people).length;
-});
+  });
 }
 
 function countPeople (films) {
   const count = films.map((film) => {
     return Object.keys(film.people).length;
-});
+  });
   count.join('')
-    const sum = count.reduce((a,b) => a + b, 0);
-    return sum;
+  const sum = count.reduce((a,b) => a + b, 0);
+  return sum;
 }
 
 function countLocations (films) {
   const count = films.map((film) => {
     return Object.keys(film.locations).length;
-});
+  });
   count.join('')
-    const sum = count.reduce((a,b) => a + b, 0);
-    return sum;
+  const sum = count.reduce((a,b) => a + b, 0);
+  return sum;
 }
 
 function countVehicles (films) {
   const count = films.map((film) => {
     return Object.keys(film.vehicles).length;
-});
+  });
   count.join('')
-    const sum = count.reduce((a,b) => a + b, 0);
-    return sum;
+  const sum = count.reduce((a,b) => a + b, 0);
+  return sum;
 }
+
+function printCataloguePeople(films) {
+  const arrayPeople = films.map((film) => {
+    const people = film.people.map((person) => {
+      const templatePerson = `
+        <div class="card-people">
+        <div class="teste">
+          <div class="div-img-people">
+            <img class="card-people-img" src="${person.img}" alt="${person.name}" name="${person.name}" >
+          </div>
+          </div>
+          <div class="person-details">
+            <h7>${person.name}</h7>
+              <p>${person.gender}</p>
+              <p>${person.age}</p>
+              <p>${person.specie}</p>
+          </div> 
+        </div>
+        `;
+      return templatePerson;
+    }); 
+  return people.join('');
+  })
+return arrayPeople.join('');
+}
+
+/* <nav class="person-details">
+<h7>${person.name}</h7><br>
+<ul>
+  <li>Gênero: ${person.gender}</li>
+  <li>Idade: ${person.age}</li>
+  <li>Espécie: ${person.specie}</li>
+</ul>
+</nav>  */
+
+// function printCatalogueLoc(films) {
+//   const arrayLoc = films.map((film) => {
+//     const locations = film.locations.map((loc) => {
+//       const templateLoc = `
+//       <div class="card-Loc">
+//       <button class= "btn-modal-loc" type="button"> 
+//       <img class="img-loc" src="${loc.img}" alt="${loc.name}" name="${loc.name}" > </button>
+//         <br><h7>${loc.name}</h7><br>
+//       </div>
+//       `;
+//       return templateLoc;
+//     }); 
+//     return loc.join('');
+//   })
+//   return arrayLoc.join('');
+// }
