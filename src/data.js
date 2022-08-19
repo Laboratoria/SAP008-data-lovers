b7dbe53fdca1b18e4f6acd0ee1506b67e9e3ddc7
devgabrielasene <gabrielasjjx@gmail.com>
2022-08-19 10:20:02 -0300

    filtro de ordem funcionando somente de A-Z

--- src/data.js
+++ src/data.js
@@ -1,10 +1,19 @@
 
-export const dataFilter = (data,typeData, condition) => data.filter((character) => {
+export const dataFilter = (data, typeData, condition) => data.filter((character) => {
     return (character[typeData] === condition);
 })
 export const nameFilter = (data, search) => data.filter(character => {
     return character.name.toLowerCase().includes(search.toLowerCase());
 })
-export const computeStats = (data,typeData, condition) => data.reduce((initialType, totalType) => {
+export const computeStats = (data, typeData, condition) => data.reduce((initialType, totalType) => {
     return initialType + (totalType[typeData] === condition);
-},0)
+}, 0)
+
+export const orderAz = (data, order) => {
+    const arrayOrder = [...data]
+    if (order === "a-z") {
+        return arrayOrder.sort((a, z) => (a.name > z.name ? 1 : -1));
+    } else {
+        return arrayOrder.sort((a, z) => (a.name < z.name ? -1 : 1));
+    }
+};
