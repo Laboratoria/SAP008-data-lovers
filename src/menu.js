34156958f3a1d814983cae92b34c1973e15beb52
brunalimxst <brunalima2160@hotmail.com>
2022-08-21 11:06:17 -0400

    responsividade

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/menu.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/close.png";
+}}
