742e81785c2420297ebb8cf6e2f2777c359b77be
Gabriela Araujo da Silva <gabrielaaraujo.sil@gmail.com>
2022-08-23 15:56:18 -0300

    filtros

--- src/data.js
+++ src/data.js
@@ -1,14 +1,41 @@
 import rickAndMortyData from './data/rickandmorty/rickandmorty.js'
 
-const getCharacters = (gender) => {
+const getCharacters = (gender, status, specie, order) => {
   let characters = rickAndMortyData.results;
-  
+
+  if (status !== '0') {
+    characters = characters.filter(character => {
+      return character.status.toLowerCase() === status.toLowerCase();
+    });
+  }
   if (gender !== '0') {
     characters = characters.filter(character => {
       return character.gender.toLowerCase() === gender.toLowerCase();
     });
   }
-  console.log(typeof gender);
+  if (specie !== '0') {
+    characters = characters.filter(character => {
+      return character.species.toLowerCase() === specie.toLowerCase();
+    });
+  }
+
+  if (order !== '0') {
+    characters = characters.sort((a, b) => {
+      if (a.name > b.name) {
+        if (order === 'crescente') {
+          return 1;
+        } else {
+          return -1;
+        }
+      } else {
+        if (order === 'crescente') {
+          return -1;
+        } else {
+          return 1;
+        }
+      }
+    });
+  }
   return characters;
 };
 
