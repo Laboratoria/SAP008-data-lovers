590ce5717fbdd81358aecf52945de8ff7dd8c515
ThaisCantarelli <thaiscantarelli@gmail.com>
2022-08-22 21:15:11 -0300

    commit:feat cards

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+function mostrarPersonagens (data){
+    document.getElementById("mostrarCards").innerHTML = data.map((personagem) => `
+    <div>
+     <p>${personagem.name}</p>
+     <img class="cards" src="${personagem.image}">
+    
+    </div>
+    
+
+    `
+ 
+
+
+).join('')
+}
+mostrarPersonagens(data.results)
+
+
+ //console.log(species, data);
