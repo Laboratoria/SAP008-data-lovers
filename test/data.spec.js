3b4361ffbb4396dac316555656e9626e56c2c29c
fabi basoni <basoni.fabiany@gmail.com>
2022-08-23 18:37:47 -0300

    updating main

--- test/data.spec.js
+++ test/data.spec.js
@@ -8,50 +8,50 @@ describe('mainFilters', () => {
 
  });
 
-describe('mainFilters.displayChars', () => {
+describe('mainFilters.displayCharactersList', () => {
 
   it('should be a function', () => {
-    expect(typeof mainFilters.displayChars).toBe('function');
+    expect(typeof mainFilters.displayCharactersList).toBe('function');
   });
 
   it('should return 707 characters', () => {
-    expect(mainFilters.displayChars().length).toBe(707)
+    expect(mainFilters.displayCharactersList().length).toBe(707)
   })
 
   it('should return Bellatrix Lestrange', () => {
-    expect(mainFilters.displayChars().includes('Bellatrix Lestrange')).toBe(true)
+    expect(mainFilters.displayCharactersList().includes('Bellatrix Lestrange')).toBe(true)
   })
 
 });
 
-describe('mainFilters.displayBooks', () => {
+describe('mainFilters.displayBooksList', () => {
 
   it('should be a function', () => {
-    expect(typeof mainFilters.displayChars).toBe('function');
+    expect(typeof mainFilters.displayCharactersList).toBe('function');
   });
 
   it('should return 8 books', () => {
-    expect(mainFilters.displayBooks().length).toBe(8)
+    expect(mainFilters.displayBooksList().length).toBe(8)
   })
 
   it('should return Harry Potter and the Prisoner of Azkaban', () => {
-    expect(mainFilters.displayBooks().includes('Harry Potter and the Prisoner of Azkaban')).toBe(true)
+    expect(mainFilters.displayBooksList().includes('Harry Potter and the Prisoner of Azkaban')).toBe(true)
   })
 
 });
 
-describe('mainFilters.displaySpells', () => {
+describe('mainFilters.displaySpellsList', () => {
 
   it('should be a function', () => {
-    expect(typeof mainFilters.displaySpells).toBe('function');
+    expect(typeof mainFilters.displaySpellsList).toBe('function');
   });
 
   it('should return 305 spells', () => {
-    expect(mainFilters.displaySpells().length).toBe(305)
+    expect(mainFilters.displaySpellsList().length).toBe(305)
   })
 
   it('should return Wingardium Leviosa', () => {
-    expect(mainFilters.displaySpells().includes('Wingardium Leviosa')).toBe(true)
+    expect(mainFilters.displaySpellsList().includes('Wingardium Leviosa')).toBe(true)
   })
 
 });
