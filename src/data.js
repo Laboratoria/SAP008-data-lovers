6b8d7ee7c391f33ef0bc2e17b7a34719db1478b2
Andrea dos Santos <contato.andreadossantos@gmail.com>
2022-08-23 11:31:44 -0300

    filtro gênero funcionando

--- src/data.js
+++ src/data.js
@@ -1,4 +1,21 @@
-// estas funciones son de ejemplo
+export const alphabeticOrder = (data) => data.sort((a,z) => a.name > z.name ? 1 : -1);
+
+export const sortByAZ = (data) => alphabeticOrder(data).slice(0, 494);
+
+export const sortByZA = (data) => sortByAZ(data).reverse();
+  
+export const filterData = (data,typeData,condition) => data.filter((character) => {
+  return (character[typeData] === condition);
+})
+
+export const filterName = (data, search) => data.filter(character => {
+  return character.name.toLowerCase().includes(search.toLowerCase());
+})
+
+export const computeStats = (data,typeData, condition) => data.reduce((initialType, totalType) => {
+ return initialType + (totalType[typeData] === condition);
+},0)
+
 
 export const example = () => {
   return 'example';
--- src/main.js
+++ src/main.js
@@ -1,6 +1,49 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
+import { computeStats, filterData} from './data.js';
 
-console.log(example, data);
+
+function printCards(data) {
+  document.getElementById('listening').innerHTML = data.map((item) => `
+  <div class="flip-container">
+     <div class="flipper">
+        <div class="front">
+           <img  class= "image" src="${item.image}">
+           <h3><strong> ${item.name}</strong></h3>
+           <p class="text">${item.species}</p>
+           <p class="text">${item.gender} * ${item.status}</p> 
+           <p class="text">${item.location.name}<p>
+        </div> 
+        <div class="back">
+        <h2>${item.name}</h2>
+        </div>
+    </div>
+</div>  
+`)
+}
+
+printCards(data.results);
+
+const selectGender = document.querySelector(".select-gender");
+const selectSpecies = document.querySelector(".select-species");
+//const selectStatus = document.querySelector(".select-status"); 
+//const searchName = document.getElementById("typed-text");
+const stats = document.querySelector(".stats");
+
+
+function printGenderFiltered() {
+  stats.style.display = "flex";
+  stats.innerHTML = `O número de personagens dessa categoria é ${computeStats(data.results, "gender", selectGender.value)}`
+  return printCards(filterData(data.results, "gender", selectGender.value));
+}
+
+function printSpeciesFiltered() {
+  stats.style.display = "flex";
+  stats.innerHTML = `O número de personagens dessa categoria é ${computeStats(data.results, "species", selectSpecies.value)}`
+  return printCards(filterData(data.results, "species", selectSpecies.value));
+}
+
+
+
+selectGender.addEventListener("change", printGenderFiltered);
+selectSpecies.addEventListener("change", printSpeciesFiltered);
+
