51f7799e2e2200be3104fe05a0bfb92a9cf1538f
ThaisCantarelli <thaiscantarelli@gmail.com>
2022-08-24 20:06:06 -0300

    commit:html

--- src/main.js
+++ src/main.js
@@ -6,21 +6,22 @@ import data from './data/rickandmorty/rickandmorty.js';
 function mostrarPersonagens (data){
     document.getElementById("mostrarCards").innerHTML = data.map((personagem) => `
     <div>
+    <img class="cards"  src="${personagem.image}">
      <p>${personagem.name}</p>
-     <img class="cards" src="${personagem.image}">
-    
+     <p>Gênero: ${personagem.gender}</p>
+     <p>Espécie: ${personagem.species}</p>
+     <p>Status: ${personagem.status}</p>
+
     </div>
     
-
     `
  
 
 
 ).join('')
 }
-
-filtro ()
 mostrarPersonagens(data.results)
+
 
 
  //console.log(species, data);
