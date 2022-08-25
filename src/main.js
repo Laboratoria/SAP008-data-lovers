c60f112b35be844e31edac36ca4fe7fe41546418
yekoshinara <leticiafs_7@hotmail.com>
2022-08-25 15:28:59 -0300

    buttonClearSearch

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import getCharacters from './data.js';
+import rickandmorty from './data/rickandmorty/rickandmorty.js';
+import rickAndMortyData from './data/rickandmorty/rickandmorty.js'
 
 let selectGender = document.getElementById('gender');
 let selectStatus = document.getElementById('status');
@@ -55,7 +57,34 @@ selectOrderAz.addEventListener('change', listarCards);
 listarCards();
 
 
-let buttonClear = document.querySelector('clear').value;
-let clearPage = buttonClear.
-buttonClear.addEventListener('click', )
+const buttonSearch = document.querySelector(".searchButton")
+buttonSearch.addEventListener("click", (e) => 
+{
+    let searchText = document.querySelector(".search").value;
+    searchName (searchText)
+})
+
+function searchName (searchTF) {
+    let characters = rickandmorty.results;
+    
+    for (const obj of characters) {
+        console.log(`${obj.name}`)
+    }
+
+    let searchText = document.querySelector(".search").value;
+
+    for (const obj of characters) {
+
+        if (obj.name.includes(searchText)) {
+        console.log("Personagem encontrado.")        
+        }
+        else {
+        console.log("Personagem não encontrado.")
+    }
+    }
+
+    
+}
+
+
 
