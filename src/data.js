c23886ab42c68bda6dd6e32b996b748f978f2b14
deboramds <deborah.mart@hotmail.com>
2022-08-26 17:13:35 -0300

    alteração background

--- src/data.js
+++ src/data.js
@@ -1,9 +1,5 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+/*export const example = () => {
   return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+}; */
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { } from './data.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+
+
+      
+      
+    
+
+      
+
+ 
+  
