d9b28118f7bdbd6c03f0cffbdd4b9d3c8d2c35cb
andresavieira28 <andresacg_14@hotmail.com>
2022-08-27 00:13:12 -0300

    Print object full

--- src/data.js
+++ src/data.js
@@ -1,23 +1,19 @@
-import data from './data/harrypotter/data.js';
-
-const filters = {
-  filterHouse,
-  filterTodos,
+import data from "./data/harrypotter/data.js";
+const filters = {filterHouse,filterAll};
+//const property = {allCharacters}
+// function allCharacters(birth){
+//   return data.characters.map((user) => user.birth === birth);
+// }
+function filterHouse(house) {
+  return data.characters.filter((user) => user.house === house);
 }
-function filterHouse(house){
-  let listHouse = data.characters.filter((user, index, array) => user.house === house)
-  let listaPersonagens = listHouse.map((user, index, array) => {return user.name})
-       
-    return listaPersonagens
-  }
+function filterAll(value) {
+  if (value === "VerTodos") {
+    let todosPersonagens = data.characters.map((user) => {
+      return user.name;
+    });
 
-function filterTodos(valor){
-  if (valor === 'VerTodos') {
-    let todosPersonagens = data.characters.map((user, index, array) => {return user.name})
-
-    return todosPersonagens
-
+    return todosPersonagens;
   }
 }
-
 export default filters;
--- src/main.js
+++ src/main.js
@@ -1,34 +1,41 @@
-//import data from './data/harrypotter/data.js';
-
 import filters from "./data.js";
-
-let select = document.querySelector('.select')
-
-select.addEventListener('change', function(event){
-    let listaNomes = filters.filterHouse(event.target.value)
-    imprimirNomes(listaNomes)
-    
-    let listaTodosPersonagens = filters.filterTodos(event.target.value)
-    imprimirNomes(listaTodosPersonagens)
-
-   })
-
-function imprimirNomes(personagensHouse){
-    for (let i = 0; i < personagensHouse.length; i++){
-       document.getElementById('lista').innerHTML += personagensHouse[i] + "</br>"
+import data from "./data/harrypotter/data.js";
+//função do botão para ver todos os personagens
+// const cast = document.querySelector('#characters')
+// cast.addEventListener('load', function(){
+//     if(target.value === 'lista'){
+//         let fullCast = data.characters()
+//         allCharacters(fullCast)
+//         document.getElementById('lista').innerHTML += fullCast + "</br>"
+//     } console.log('teste')
+// })
+window.addEventListener('load', function (event){
+    let cast = data.characters 
+    allCharacters(cast)
+   console.log('teste')
+})
+// function allCharacters(cast){
+//     for (let i = 0; i < cast.length; i++) {
+//         console.log(cast[i])
+//        document.getElementById('lista').innerHTML += cast[i] + "<br>"   
+//    }
+// }
+//função do select personagens ver todos, filtros por casa   
+let selectCharacters = document.querySelector('.select')
+selectCharacters.addEventListener('change', function(event){
+    if(event.target.value === "VerTodos"){
+        let listAllCharacters = filters.filterAll(event.target.value)
+        printName(listAllCharacters)
+    }else{
+    let listName = filters.filterHouse(event.target.value)
+    printName(listName)
+}
+})     
+function printName(charactersHouse){
+    console.log(charactersHouse)
+    document.getElementById('lista').innerHTML = ""
+    for (let i = 0; i < charactersHouse.length; i++){
+       document.getElementById('lista').innerHTML += charactersHouse[i] + "</br>"
     }
-    }
-   
-
-// const gryffindor = data.characters.filter((user, index, array) => user.house === 'Gryffindor');
-// const slytherin = data.characters.filter((user, index, array) => user.house === 'Slytherin');
-// const ravenclaw = data.characters.filter((user, index, array) => user.house === 'Ravenclaw');
-// const hufflepuff = data.characters.filter((user, index, array) => user.house === 'Hufflepuff');
-
-// const totalHouses = gryffindor.length + slytherin.length + ravenclaw.length + hufflepuff.length
-
-//console.log(listaPersonagens)
-// console.log((gryffindor.length / totalHouses)*100 + " %")
-// console.log((slytherin.length / totalHouses)*100 + " %")
-// console.log((ravenclaw.length / totalHouses)*100 + " %")
-// console.log((hufflepuff.length / totalHouses)*100 + " %")
+    console.log('printName')
+}
