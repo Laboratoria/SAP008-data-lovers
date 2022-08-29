1cea8def083837c3ece32fe441cb3e4e0008110f
andresavieira28 <andresacg_14@hotmail.com>
2022-08-29 18:19:00 -0300

    refresh characters function

--- src/data.js
+++ src/data.js
@@ -1,19 +1,20 @@
-import data from "./data/harrypotter/data.js";
-const filters = {filterHouse,filterAll};
-//const property = {allCharacters}
-// function allCharacters(birth){
-//   return data.characters.map((user) => user.birth === birth);
-// }
+export default {filters};
+
+const filters = {displayList,filterHouse, filterAll};
+
+function displayList(name){
+  return name.map((characters) => characters.name)
+}
 function filterHouse(house) {
-  return data.characters.filter((user) => user.house === house);
+  return filters.characters.filter((user) => user.house === house);
 }
 function filterAll(value) {
   if (value === "VerTodos") {
-    let todosPersonagens = data.characters.map((user) => {
+    let todosPersonagens = filters.characters.map((user) => {
       return user.name;
     });
 
     return todosPersonagens;
   }
 }
-export default filters;
+//import data from "./data/harrypotter/data.js";
--- src/main.js
+++ src/main.js
@@ -1,26 +1,13 @@
-import filters from "./data.js";
 import data from "./data/harrypotter/data.js";
-//função do botão para ver todos os personagens
-// const cast = document.querySelector('#characters')
-// cast.addEventListener('load', function(){
-//     if(target.value === 'lista'){
-//         let fullCast = data.characters()
-//         allCharacters(fullCast)
-//         document.getElementById('lista').innerHTML += fullCast + "</br>"
-//     } console.log('teste')
-// })
-window.addEventListener('load', function (event){
-    let cast = data.characters 
-    allCharacters(cast)
-   console.log('teste')
-})
-// function allCharacters(cast){
-//     for (let i = 0; i < cast.length; i++) {
-//         console.log(cast[i])
-//        document.getElementById('lista').innerHTML += cast[i] + "<br>"   
-//    }
-// }
-//função do select personagens ver todos, filtros por casa   
+import filters from "./data.js";
+const fullCast = data.characters;
+const btnCharacters = document.querySelector('#charactersFull')
+btnCharacters.addEventListener('click',
+function displayCharaList(){
+    const charactersName = filters.displayList(fullCast);
+    const charactersPrint = document.getElementById('lista');
+    return charactersPrint.innerHTML = charactersName.map(name => `<ul><li> ${name}</li></ul>`).join('');
+})  
 let selectCharacters = document.querySelector('.select')
 selectCharacters.addEventListener('change', function(event){
     if(event.target.value === "VerTodos"){
@@ -37,5 +24,4 @@ function printName(charactersHouse){
     for (let i = 0; i < charactersHouse.length; i++){
        document.getElementById('lista').innerHTML += charactersHouse[i] + "</br>"
     }
-    console.log('printName')
 }
