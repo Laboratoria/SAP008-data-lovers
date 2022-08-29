19421c7740af781983c9f2afc79a389382a15eb0
Kauane <kauanelenzr@gmail.com>
2022-08-29 16:01:45 -0300

    cards dos filmes printados na tela

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { example } from './data.js';
+//import {} from './data.js';
 
-import data from './data/ghibli/ghibli.js';
+import data from "./data/ghibli/ghibli.js";
 
-console.log(example, data);
+const ghibliMovies = data.films;
+
+cardsMovies(ghibliMovies);
+
+function cardsMovies(movies) {
+  const printCard = movies.map((films) => {
+    return `
+<div class="cardContainer">
+  <div class="image">
+    <img class="picture" src="${films.poster}" alt="movie picture">
+  </div>
+  <div class="movieName">${films.title}</div>
+  <ul class="info">
+    <li>Description: ${films.description}</li>
+    <li>Release date: ${films.release_date}</li>
+    <li>Director: ${films.director}</li>
+    <li>Producer: ${films.producer}</li>
+  </ul>
+</div>
+`;
+  });
+  document.getElementById('cards-movies').innerHTML = printCard.join("")
+}
 
