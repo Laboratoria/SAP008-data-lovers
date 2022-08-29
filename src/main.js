b97c4a53852ab01d34a1c4b1e07fa143b36eb1b7
Andrea dos Santos <contato.andreadossantos@gmail.com>
2022-08-29 13:40:52 -0300

    testes iniciados sortByAZ, sortByZA, filterData

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ document.getElementById('listening').innerHTML = mapa.join(" ");
 
 printCards(data.results);
 
-const selectEpisode = document.querySelector(".selectEpisode")
+//const selectEpisode = document.querySelector(".selectEpisode")
 const selectGender = document.querySelector(".select-gender");
 const selectSpecies = document.querySelector(".select-species");
 const selectStatus = document.querySelector(".select-status"); 
@@ -77,7 +77,7 @@ selectSpecies.addEventListener("change", printSpeciesFiltered);
 selectStatus.addEventListener("change", printStatusFiltered);
 searchName.addEventListener("keypress", printFilterByName);
 searchName.addEventListener("keydown", event => {
-   if (eventKeyCode=== 8) {
+   if (event.KeyCode=== 8) {
      return printCards(data.results);
    }
 })
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,78 @@
-import { example, anotherExample } from '../src/data.js';
+import {sortByAZ, sortByZA,filterData, filterName} from '../src/data.js';
 
+const characters = [
+  { 
+    "id": 1,
+   "name": "Rick Sanchez",
+   "status": "Alive",
+   "species": "Human",
+   "gender": "Male",
+  },
+{
+    "id": 3,
+    "name": "Summer Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Female", 
+  },
+  {
+    "id": 13,
+    "name": "Alien Googah",
+    "status": "unknown",
+    "species": "Alien",
+    "gender": "unknown", 
+  }
+  
+]
 
-describe('example', () => {
+describe("sortByAZ", () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof sortByAZ).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('deve ordenar um array por ordem alfabética', () => {
+    const ordenado = sortByAZ(characters)    
+    expect(ordenado.length).toBe(characters.length);
+    expect(ordenado).toBe([characters[2], characters[0], characters[1]]);
+  });
+});
+
+describe("sortByZA", () => {
+  it('is a function', () => {
+    expect(typeof sortByZA).toBe('function');
+  });
+
+  it('deve ordenar um array pela ordem de Z a A', () => {
+    const sortByZA = sortByAZ(characters).reverse();   
+    expect(sortByZA).toBe([characters[1], characters[0], characters[2]]);
+  });
+});
+
+describe("filterData", () => {
+  it('is a function', () => {
+    expect(typeof filterData).toBe('function');
+  });
+
+  it('deve filtrar pelo os dados de gender, status e species', () => {
+    const filtrar = filterData(characters);   
+    expect(filtrar.length).toBe(characters.length);
+    expect(filtrar).toBe([characters["gender"]]);
+    expect(filtrar).toBe([characters["status"]]);
+    expect(filtrar).toBe([characters["species"]]);
+  });
+});
+
+describe("filterName", () => {
+  it('is a function', () => {
+    expect(typeof filterName).toBe('function');
+  });
+
+  it('deve pesquisar pelo nome', () => {
+    const pesquisar = filterName(characters);   
+    expect(pesquisar.length).toBe(characters.length);
+    expect(pesquisar).toBe([characters["name"]]);
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
