4f0794ef9ea17dec6abf71b61e3ca0d26e220979
AghathaK <aghatha.kriss01@gmail.com>
2022-08-29 14:24:12 -0300

    INSERÇÃO DE CARDS NA TELA

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-import {data} from "./data/rickandmorty/rickandmorty";
+import { example } from './data.js';
+// import data from './data/lol/lol.js';
+import data from './data/rickandmorty/rickandmorty.js';
+// import data from './data/rickandmorty/rickandmorty.js';
+
+const cardsContainer = document.querySelector("#cardsContainer")
+
+function gerarCards(cards){  
+    const arrayCards = cards.map((item) => {
+        const cardsList = `
+        <div class="cards">
+            <img class="cardsImg" src="${item.image}" alt="${item.name}">
+            <ul class="card-text" style="list-style: none">                       
+            <li>Name: ${item.name}</li>
+            <li>Status: ${item.status}</li>
+            <li>Species: ${item.species}</li>
+            <li>Gender: ${item.gender}</li>
+            <li>Location: ${item.origin.name}</li>
+            </ul>                
+        </div>
+        `;
+        return cardsList;
+       
+    }).join("")
+    
+    cardsContainer.innerHTML += arrayCards
+};
+    
+gerarCards(data.results);
