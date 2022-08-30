9c64cbe4047c5d80e74026cf0ba996bbbbb846e3
deboramds <deborah.mart@hotmail.com>
2022-08-30 13:08:18 -0300

    Reparos nos prints

--- src/data.js
+++ src/data.js
@@ -8,3 +8,40 @@ export const filterFilms = (films) =>{
 
   return ' ';
 }
+
+export function printFilmsCards(data ) {
+  return data.map((item) =>
+    `
+  <div class="cards">
+    <div class="cardFlip">
+      <div class="front">
+        <figure>
+          <img src = "${item.poster}" class = "poster">
+        </figure>
+      </div>
+      <div class="back">
+        <p class = "titles"><strong>${item.title} </strong></p><br>
+        <p class = "infoFilms"><strong>Release Date: </strong>${item.release_date}</p><br>
+        <p class = "infoFilms"><strong>Description: </strong>${item.description}</p><br>
+        <p class = "infoFilms"><strong>Director: </strong>${item.director}<p><br>
+        <p class = "infoFilms"><strong>Producer: </strong>${item.producer}<p><br>
+        <p class = "infoFilms"><strong>Score: </strong>${item.rt_score}<p><br>
+      </div>
+     </div>
+    </div>
+    `
+    )
+  .join('');
+
+}
+
+
+export function getCharacters (films){
+  let characters =[] 
+  for (let i = 0; i < films.length; i++ ){
+    characters=characters.concat(films[i].people)
+
+  } 
+  return characters 
+
+}
--- src/main.js
+++ src/main.js
@@ -1,35 +1,17 @@
-import { filterFilms } from './data.js';
+import { filterFilms, printFilmsCards, getCharacters } from './data.js';
 
 import data from './data/ghibli/ghibli.js';
 
 const films = data.films
 
-function printCards(data ) {
-  document.getElementById('cardsFilms').innerHTML = data.map((item) =>
-    `
-  <div class="cards">
-    <div class="cardFlip">
-      <div class="front">
-        <figure>
-          <img src = "${item.poster}" class = "poster">
-        </figure>
-      </div>
-      <div class="back">
-        <p class = "titles"><strong>${item.title} </strong></p><br>
-        <p class = "infoFilms"><strong>Release Date: </strong>${item.release_date}</p><br>
-        <p class = "infoFilms"><strong>Description: </strong>${item.description}</p><br>
-        <p class = "infoFilms"><strong>Director: </strong>${item.director}<p><br>
-        <p class = "infoFilms"><strong>Producer: </strong>${item.producer}<p><br>
-        <p class = "infoFilms"><strong>Score: </strong>${item.rt_score}<p><br>
-      </div>
-     </div>
-    </div>
-    `
-    )
-  .join('');
-}
 
-printCards(data.films)
+function showFilmsCards () { 
+  document.getElementById('cardsFilms').innerHTML = printFilmsCards(data.films)
+}
+console.log (getCharacters (data.films))
+
+
+showFilmsCards()
 
 const searchMovie = () =>{
   const valueSelec = searchFilms.value;
@@ -40,14 +22,3 @@ const searchMovie = () =>{
 const searchFilms = document.querySelector("#filtro");
 searchFilms.addEventListener("keyup", searchMovie);
 
-
-
-
-      
-      
-    
-
-      
-
- 
-  
