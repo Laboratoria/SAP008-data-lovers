4ace8dfc1d25c304df20a5d72fba289e53bf8492
Thainaprado7 <thaina.prado.psic@gmail.com>
2022-08-30 16:58:05 -0300

    js do menu responsivo

--- src/main.js
+++ src/main.js
@@ -4,3 +4,20 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+function clickMenu(){
+    if(paginas.style.display == 'block'){
+       paginas.style.display = 'none'
+    } else {
+       paginas.style.display = 'block'
+    }
+  }
+  
+  function responsivo() {
+    if(window.innerWidth >= 768) {
+      paginas.style.display = 'block'
+    } else {
+      paginas.style.display = 'none'
+    }
+  }
+  
