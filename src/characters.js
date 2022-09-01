aff05c20c2d35b9ac7877e942eec39847aae1754
Natália Evelyn <natalia_evelyn@icloud.com>
2022-09-01 14:05:16 -0300

    Introdução do código JS para trazer cards dos personagens

--- src/characters.js
+++ src/characters.js
@@ -1,69 +1,28 @@
 import data from "./data/ghibli/ghibli.js";
 
-cardsPeople(data.films);
-function cardsPeople(films) {
-    let cardsArea = document.getElementById('cards-characters')
-    films.map(film =>
-        film.people.map(person =>{
-             console.log(`
-    ${person.name}
-    ${person.age}
-    ${person.gender}
-    ${person.specie}
-            `)
-        }))
+function cardsCharacters(films) {
+  let characters = []
+  films.map(film =>{
+    film.people.map(person => {
+      characters.push(person)
+    })
+  })
+ characters.map((card) => {
+  return `
+  <div class="cardContainer">
+   <div class="image">
+     <img class="picture" src="${card.img}" alt="character picture">
+   </div>
+   <div class="name">${card.name}</div>
+   <ul class="info">
+     <li>age: ${card.age}</li>
+     <li>gender: ${card.gender}</li>
+     <li>specie: ${card.specie}</li>
+   </ul>
+ </div>
+  `
+ })
+ document.getElementById('cards-characters').innerHTML = characters.join("")
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const dataBase = data.films
-// cardsPeople(dataBase)
-// function cardsPeople(dataBase) {
-// let result = document.getElementById('cards-characters')
-//  dataBase.map(film => {
-//   film.people.map(person => { 
-//     return result.innerHTML =  `
-//     <div class="cardContainer">
-//   <div class="image">
-//     <img class="picture" src="${person.img}" alt="movie picture">
-//   </div>
-//   <div class="characterName">${person.name}</div>
-//   <ul class="info">
-//     <li>Age: ${person.age}</li>
-//     <li>Gender: ${person.gender}</li>
-//     <li>Specie: ${person.specie}</li>
-//   </ul>
-// </div>
-//     `
-//   })
-  
-// })
-// }
-
-
-
-
-
-// cardsPeople(data.films);
-
-// function cardsPeople(films) {
-// let characters = []
-// films.map(film => {
-//   film.people.map(person => { 
-//     characters.push(person)
-//   })
-// })
-// console.log(characters)
-// }
- 
-
+cardsCharacters(data.films)
