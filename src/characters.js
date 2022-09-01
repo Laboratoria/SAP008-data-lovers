57513e9170d2480f8d7927c239e275e634eefecf
raquelthabata <r.thabata05@gmail.com>
2022-09-01 15:08:35 -0300

    js

--- src/characters.js
+++ src/characters.js
@@ -1,10 +1,1 @@
-/*import data from "./data/ghibli/ghibli.js"
-function getCharacters (films){
-    let characters = []
-    for (let i= 0; i<films.length; i++){
-        characters = characters.concat(movies[i].people)
-    }
-    console.log(characters)
-    return characters
-}
-getCharacters (data.films) */
+
