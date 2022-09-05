bc1da4cc60ba7519151d0a9b167dcea79069fb66
taliithaanlemes@gmail.com <64807282+TalithaNeves@users.noreply.github.com>
2022-09-05 15:06:14 -0300

    responsividades

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,129 +1,9 @@
 import {
+  filterArr,
   orderCrescente,
   ordemDecrescente,
-  filterArr,
   porcentagemCal,
 } from "../src/data.js";
-
-it("orderCrescente", () => {
-  const mockReturn = [
-    {
-      gender: "Female",
-      id: 6,
-      image:
-        "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/6.jpeg",
-      location: {
-        name: "Abadango",
-        url: "https://rickandmortyapi.com/api/location/2",
-      },
-      name: "Abadango Cluster Princess",
-      origin: {
-        name: "Abadango",
-        url: "https://rickandmortyapi.com/api/location/2",
-      },
-      species: "Alien",
-      status: "Alive",
-      type: "",
-    },
-    {
-      gender: "Male",
-      id: 2,
-      image:
-        "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/2.jpeg",
-      location: {
-        name: "Earth (Replacement Dimension)",
-        url: "https://rickandmortyapi.com/api/location/20",
-      },
-      name: "Morty Smith",
-      origin: {
-        name: "Earth (C-137)",
-        url: "https://rickandmortyapi.com/api/location/1",
-      },
-      species: "Human",
-      status: "Alive",
-      type: "",
-    },
-    {
-      gender: "Male",
-      id: 1,
-      image:
-        "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/1.jpeg",
-      location: {
-        name: "Earth (Replacement Dimension)",
-        url: "https://rickandmortyapi.com/api/location/20",
-      },
-      name: "Rick Sanchez",
-      origin: {
-        name: "Earth (C-137)",
-        url: "https://rickandmortyapi.com/api/location/1",
-      },
-      species: "Human",
-      status: "Alive",
-      type: "",
-    },
-  ];
-  expect(mockList.sort(orderCrescente)).toMatchObject(mockReturn);
-});
-
-it("ordemDecrescente", () => {
-  const mockReturn = [
-    {
-      gender: "Male",
-      id: 1,
-      image:
-        "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/1.jpeg",
-      location: {
-        name: "Earth (Replacement Dimension)",
-        url: "https://rickandmortyapi.com/api/location/20",
-      },
-      name: "Rick Sanchez",
-      origin: {
-        name: "Earth (C-137)",
-        url: "https://rickandmortyapi.com/api/location/1",
-      },
-      species: "Human",
-      status: "Alive",
-      type: "",
-    },
-    {
-      gender: "Male",
-      id: 2,
-      image:
-        "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/2.jpeg",
-      location: {
-        name: "Earth (Replacement Dimension)",
-        url: "https://rickandmortyapi.com/api/location/20",
-      },
-      name: "Morty Smith",
-      origin: {
-        name: "Earth (C-137)",
-        url: "https://rickandmortyapi.com/api/location/1",
-      },
-      species: "Human",
-      status: "Alive",
-      type: "",
-    },
-    {
-      gender: "Female",
-      id: 6,
-      image:
-        "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/6.jpeg",
-      location: {
-        name: "Abadango",
-        url: "https://rickandmortyapi.com/api/location/2",
-      },
-      name: "Abadango Cluster Princess",
-      origin: {
-        name: "Abadango",
-        url: "https://rickandmortyapi.com/api/location/2",
-      },
-      species: "Alien",
-      status: "Alive",
-      type: "",
-    },
-  ];
-  expect(mockList.sort(ordemDecrescente)).toMatchObject(mockReturn);
-});
 
 describe("filtrar personagens vivos", () => {
   const mockList = [
@@ -208,9 +88,129 @@ describe("filtrar personagens vivos", () => {
     expect(filterArr(mockList, "species", "Alien")).toMatchObject(mockReturn);
   });
 
+  it("orderCrescente", () => {
+    const mockReturn = [
+      {
+        gender: "Female",
+        id: 6,
+        image:
+          "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/6.jpeg",
+        location: {
+          name: "Abadango",
+          url: "https://rickandmortyapi.com/api/location/2",
+        },
+        name: "Abadango Cluster Princess",
+        origin: {
+          name: "Abadango",
+          url: "https://rickandmortyapi.com/api/location/2",
+        },
+        species: "Alien",
+        status: "Alive",
+        type: "",
+      },
+      {
+        gender: "Male",
+        id: 2,
+        image:
+          "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/2.jpeg",
+        location: {
+          name: "Earth (Replacement Dimension)",
+          url: "https://rickandmortyapi.com/api/location/20",
+        },
+        name: "Morty Smith",
+        origin: {
+          name: "Earth (C-137)",
+          url: "https://rickandmortyapi.com/api/location/1",
+        },
+        species: "Human",
+        status: "Alive",
+        type: "",
+      },
+      {
+        gender: "Male",
+        id: 1,
+        image:
+          "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/1.jpeg",
+        location: {
+          name: "Earth (Replacement Dimension)",
+          url: "https://rickandmortyapi.com/api/location/20",
+        },
+        name: "Rick Sanchez",
+        origin: {
+          name: "Earth (C-137)",
+          url: "https://rickandmortyapi.com/api/location/1",
+        },
+        species: "Human",
+        status: "Alive",
+        type: "",
+      },
+    ];
+    expect(mockList.sort(orderCrescente)).toMatchObject(mockReturn);
+  });
+
+  it("ordemDecrescente", () => {
+    const mockReturn = [
+      {
+        gender: "Male",
+        id: 1,
+        image:
+          "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/1.jpeg",
+        location: {
+          name: "Earth (Replacement Dimension)",
+          url: "https://rickandmortyapi.com/api/location/20",
+        },
+        name: "Rick Sanchez",
+        origin: {
+          name: "Earth (C-137)",
+          url: "https://rickandmortyapi.com/api/location/1",
+        },
+        species: "Human",
+        status: "Alive",
+        type: "",
+      },
+      {
+        gender: "Male",
+        id: 2,
+        image:
+          "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/2.jpeg",
+        location: {
+          name: "Earth (Replacement Dimension)",
+          url: "https://rickandmortyapi.com/api/location/20",
+        },
+        name: "Morty Smith",
+        origin: {
+          name: "Earth (C-137)",
+          url: "https://rickandmortyapi.com/api/location/1",
+        },
+        species: "Human",
+        status: "Alive",
+        type: "",
+      },
+      {
+        gender: "Female",
+        id: 6,
+        image:
+          "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/6.jpeg",
+        location: {
+          name: "Abadango",
+          url: "https://rickandmortyapi.com/api/location/2",
+        },
+        name: "Abadango Cluster Princess",
+        origin: {
+          name: "Abadango",
+          url: "https://rickandmortyapi.com/api/location/2",
+        },
+        species: "Alien",
+        status: "Alive",
+        type: "",
+      },
+    ];
+    expect(mockList.sort(ordemDecrescente)).toMatchObject(mockReturn);
+  });
+
   it("porcentagemCal", () => {
     expect(porcentagemCal(100, 490)).toBe("20%");
     expect(porcentagemCal(250, 490)).toBe("51%");
     expect(porcentagemCal(90, 490)).toBe("18%");
   });
-});
+})
