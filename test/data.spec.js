b68d3a146e9e6f7ead3be4e7c6305a3e78f2699a
Tamyres <tamyresfmelo@gmail.com>
2022-09-06 17:43:54 -0300

    teste selecNameAz, selecNameZa e filterFind

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,40 @@
 
 import {createCard, filterHouse, countCharactersHogwarts, selectNameAz, selectNameZa, filterFind} from '../src/data.js';
 
+const HarryPotterData = [
+{
+  name: "Hermione",
+  house: "Gryffindor"
+},
+{
+  name: "Draco",
+  house: "Slytherin"
+},
+{
+  name: "Luna",
+  house: "Hufflepuff"
+}]
+
 describe('Teste createCard', () => {
   it('is a function', () => {
     expect(typeof createCard).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(example()).toBe('OMG');
-  });
+  // it('returns `anotherExample`', () => {
+  //   expect(example()).toBe('OMG');
+  // });
 });
 
 describe('Teste FilterHouse', () => {
   it('is a function', () => {
     expect(typeof filterHouse).toBe('function');
-  });
+  }); 
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('should returns "[{ Name: "Hermione", House: "Gryffindor"}]" ', () => {
+    expect(filterHouse(HarryPotterData)).toBe([{
+      name: "Hermione",
+      house: "Gryffindor"
+    }]);
   });
 });
 
@@ -26,35 +43,34 @@ describe('Teste countCharactersHogwarts', () => {
     expect(typeof countCharactersHogwarts).toBe('function');
   });
 
-  const HarryPotterData = [
-    {
-      Nome: "Hermione",
-      House: "Gryffindor"
-  },
-  {
-    Nome: "Draco",
-    House: "Slytherin"
-  },
-  {
-    Nome: "Luna",
-    House: "Hufflepuff"
-  }]
-
-  it('should returns "[{ Name: "Hermione", House: "Gryffindor"}]" ', () => {
-    expect(filterHouse(HarryPotterData)).toBe([{
-      Nome: "Hermione",
-      House: "Gryffindor"
-    }]);
-  });
-});
+  // it('returns `example`', () => {
+  //   expect(example()).toBe('example');
+  // });  
+})
 
 describe('Teste selectNameAz', () => {
   it('is a function', () => {
     expect(typeof selectNameAz).toBe('function');
   });
 
+  const HarryPotterAz = [
+  {
+    name: "Harry",
+    house: "Gryffindor"
+  },
+  {
+    name: "Severo",
+    house: "Slytherin"
+  },
+  {
+    name: "Cedrico",
+    house: "Hufflepuff"
+  }]
+
+  const listaOrdenada = selectNameAz(HarryPotterAz)
+
   it('returns `anotherExample`', () => {
-    expect(example()).toBe('OMG');
+    expect(listaOrdenada[0].name).toBe('Cedrico');
   });
 });
 
@@ -63,8 +79,24 @@ describe('Teste selectNameZa', () => {
     expect(typeof selectNameZa).toBe('function');
   });
 
+  const HarryPotterZa = [
+  {
+    name: "Lucio",
+    house: "Slytherin"
+  },
+  {
+    name: "Rony",
+    house: "Gryffindor"
+  },
+  {
+    name: "Newton",
+    house: "Hufflepuff"
+  }]
+
+  const listaOrdenadaZa = selectNameZa(HarryPotterZa)
+
   it('returns `anotherExample`', () => {
-    expect(example()).toBe('OMG');
+    expect(listaOrdenadaZa[0].name).toBe('Rony');
   });
 });
 
@@ -73,7 +105,10 @@ describe('Teste filterFind', () => {
     expect(typeof filterFind).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(example()).toBe('OMG');
+  it('should returns "[{ Name: "Hermione", House: "Gryffindor"}]" ', () => {
+    expect(filterFind('Hermione', HarryPotterData)).toStrictEqual([{
+      name: "Hermione",
+      house: "Gryffindor"
+    }]);
   });
-});
+})
