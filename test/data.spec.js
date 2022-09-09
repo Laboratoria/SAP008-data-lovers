3ba0656cb6c8f3cc4b461f9b33e1efbf86614299
andradeannac <andradeannac@gmail.com>
2022-09-09 16:44:28 -0300

    test inicial

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,112 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterGenero, filterStatus, filterEspecie, filterNome, sortByName, computeStats } from '../src/data.js';
+const personagens = [
+  {
+    "id": 1,
+    "name": "Rick Sanchez",
+    "status": "Alive",
+    "species": "Human",
+    "type": "",
+    "gender": "Male",
+  },
+  {
+    "id": 2,
+    "name": "Morty Smith",
+    "status": "Alive",
+    "species": "Human",
+    "type": "",
+    "gender": "Male",
 
+  },
+  {
+    "id": 3,
+    "name": "Summer Smith",
+    "status": "Alive",
+    "species": "Human",
+    "type": "",
+    "gender": "Female",
+  }
+]
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+// filtro genero
+describe("filtroGenero", () => {
+  it("Deverá ser uma função", () => {
+    expect(typeof filterGenero).toBe("function");
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it("Devera filtrar o genero selecionado", () => {
+    const retornoPersonagens = [personagens[2]]
+    expect(filterGenero(personagens, "Female")).toEqual(retornoPersonagens)
+  })
+});
+// filtro status
+describe("filtro Status", () => {
+  it("Deverá ser uma função", () => {
+    expect(typeof filterStatus).toBe("function");
   });
+
+  it("Devera filtrar o status selecionado", () => {
+    const retornoPersonagens = [personagens[0], personagens[1], personagens[2]]
+    expect(filterStatus(personagens, "Alive")).toEqual(retornoPersonagens)
+  })
+});
+// filtro especie
+describe("filtro Especie", () => {
+  it("Deverá ser uma função", () => {
+    expect(typeof filterEspecie).toBe("function");
+  });
+
+  it("Devera filtrar a especie selecionado", () => {
+    const retornoPersonagens = [personagens[0], personagens[1], personagens[2]]
+    expect(filterEspecie(personagens, "Human")).toEqual(retornoPersonagens)
+  })
+});
+//filtro nome
+describe("filtro nome", () => {
+  it("Deverá ser uma função", () => {
+    expect(typeof filterEspecie).toBe("function");
+
+  });
+
+  it("Devera filtrar o nome digitado", () => {
+    const retornoPersonagens = [personagens[1]]
+    expect(filterNome(personagens, "Morty Smith")).toEqual(retornoPersonagens)
+  })
 });
 
+// ordernar de a-z
+const nome = [
+  {
+    "name": "Rick Sanchez",
+  },
+  {
+    "name": "Morty Smith",
+  },
+  {
+    "name": "Summer Smith",
+  },
+]
+describe("ordernar de A-z", () => {
+  it("Deverá ser uma função", () => {
+    expect(typeof sortByName).toBe("function");
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it("Devera ordenar de A-z como selecionado pelo select", () => {
+    expect(sortByName(nome, "ordenarAZ")).toStrictEqual(nome)
+  })
+  it("Devera retornar  de Z-a", () => {
+    expect(sortByName(nome, "ordenarZA")).toStrictEqual(nome)
+  })
+});
+
+// calculo
+describe("calcular quantidade total", () => {
+  it("Deverá ser uma função", () => {
+    expect(typeof computeStats).toBe("function");
   });
+
+  it("Devera retornar a quantidade total do select escolhido", () => {
+    const retornoPersonagens = [personagens[2]]
+    expect(computeStats(retornoPersonagens)).toEqual(1)
+  })
 });
