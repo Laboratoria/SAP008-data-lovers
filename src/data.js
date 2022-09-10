85cadca6fa4ee2ffc1034105c03aad012b723f4c
adevcarol <alvscarolina@outlook.com>
2022-09-10 04:12:58 -0300

    filtro de países e ajustes no filtro de modalidades

--- src/data.js
+++ src/data.js
@@ -1,9 +1,112 @@
-const medalIcon = [{
-  "Gold": "🥇",
-  "Silver": "🥈",
-  "Bronze": "🥉"
-}]
+const medalIcon = ['🥇', '🥈', '🥉']
 
+/* ----------------FILTRO DAS MEDALHAS-------------------------- */
+export const medalFilter = (data, condition, subcondition) => {
+  const medal = data.map((element) => {
+    if (element.medal == condition && (element.sport == subcondition || element.team == subcondition)) {
+
+      return `
+            <div class="card-atleta" id="card-atleta">
+                <div class="card-efeito" >
+                    <div class="card-frente">
+                      <p class="nome"><strong>Nome:</strong> ${element.name}</p>
+                    </div>
+                    <div class="card-verso">    
+                        <div class="atributos-atleta">
+                            <p class="genero"><strong>Gênero:</strong> ${element.gender}</p>
+                            <p class="esporte"><strong>Modalidade:</strong> ${element.sport}</p>
+                            <p class="pais"><strong>País:</strong> ${element.team}</p>
+                            <p class="medalha"><strong>Medalha:</strong> ${element.medal} ${medalIcon[0]}</p>
+                            <p class="evento"><strong>Categoria:</strong> ${element.event}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+  }).join("")
+  return medal;
+};
+
+/* ----------------FILTRO DAS MODALIDADES-------------------------- */
+/* função para caregar os paises no select */
+export function stringSport(data) {
+  const item = data.map((element) => {
+    return `
+            <option value="${element.sport}">${element.sport}</option>
+          `
+  });
+  return [...new Set(item)].join(""); // ...new Set para remover itens repedidos do array
+};
+
+/* função para filtrar por pais */
+export function filterSport(data, condition) {
+  const cards = data.map((element) => {
+    if (condition == element.sport) {
+      return `
+            <div class="card-atleta" id="card-atleta">
+                <div class="card-efeito" >
+                    <div class="card-frente">
+                      <p class="nome"><strong>Nome:</strong> ${element.name}</p>
+                    </div>
+                    <div class="card-verso">    
+                        <div class="atributos-atleta">
+                            <p class="genero"><strong>Gênero:</strong> ${element.gender}</p>
+                            <p class="esporte"><strong>Modalidade:</strong> ${element.sport}</p>
+                            <p class="pais"><strong>País:</strong> ${element.team}</p>
+                            <p class="medalha"><strong>Medalha:</strong> ${element.medal} ${medalIcon[0]}</p>
+                            <p class="evento"><strong>Categoria:</strong> ${element.event}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+  }).join("")
+  return cards;
+};
+/* ------------------------------------------------------- */
+
+/* ----------------FILTRO DOS PAÌSES-------------------------- */
+/* função para caregar os paises no select */
+export function stringTeam(data) {
+  const item = data.map((element) => {
+    return `
+            <option value="${element.team}">${element.team}</option>
+          `
+  });
+  return [...new Set(item)].join(""); // ...new Set para remover itens repedidos do array
+};
+
+/* função para filtrar por pais */
+export function filterTeam(data, condition) {
+  const cards = data.map((element) => {
+    if (condition == element.team) {
+      return `
+            <div class="card-atleta" id="card-atleta">
+                <div class="card-efeito" >
+                    <div class="card-frente">
+                      <p class="nome"><strong>Nome:</strong> ${element.name}</p>
+                    </div>
+                    <div class="card-verso">    
+                        <div class="atributos-atleta">
+                            <p class="genero"><strong>Gênero:</strong> ${element.gender}</p>
+                            <p class="esporte"><strong>Modalidade:</strong> ${element.sport}</p>
+                            <p class="pais"><strong>País:</strong> ${element.team}</p>
+                            <p class="medalha"><strong>Medalha:</strong> ${element.medal} ${medalIcon[0]}</p>
+                            <p class="evento"><strong>Categoria:</strong> ${element.event}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+  }).join("");
+  return cards;
+}
+/* ------------------------------------------------------- */
+
+/* ------------------------------------------------------- */
 // esta função receberia os dados e nos retornaria os que cumprem com a condição.
 export const filterData = (data, condition) => {
   const cards = data.map((element) => {
@@ -18,16 +121,21 @@ export const filterData = (data, condition) => {
                             <p class="genero"><strong>Gênero:</strong> ${element.gender}</p>
                             <p class="esporte"><strong>Modalidade:</strong> ${element.sport}</p>
                             <p class="pais"><strong>País:</strong> ${element.team}</p>
-                            <p class="medalha"><strong>Medalha:</strong> ${element.medal} ${medalIcon[0].Gold}</p>
+                            <p class="medalha"><strong>Medalha:</strong> ${element.medal} ${medalIcon[0]}</p>
                             <p class="evento"><strong>Categoria:</strong> ${element.event}</p>
                         </div>
                     </div>
                 </div>
             </div>
         `
-  }).join("")
-  return cards
+  }).join("");
+  return cards;
 }
+
+
+
+
+
 
 // esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
 export const sortData = (data, sortBy, sortOrder) => {
@@ -36,7 +144,7 @@ export const sortData = (data, sortBy, sortOrder) => {
   document.getElementById("presentes").innerHTML = "";
 
   for (var i = 0; i < convidados.length; i++) {
-    return`
+    return `
       
     `
   }
@@ -44,33 +152,7 @@ export const sortData = (data, sortBy, sortOrder) => {
 
 // essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.
 export const computeStats = (data) => {
-  //...
+  let test = data.reduce(function (acumulador, valorAtual) {
+    return acumulador + valorAtual;
+  }, 0)
 }
-
-/*
-export const filterData = (data) => {
-  let cardView = "";
-  const cards = data.map((element) => {
-    return `
-            <div class="card-atleta" id="card-atleta">
-                <div class="card-efeito" >
-                    <div class="card-frente">
-                    
-                    </div>
-                    <div class="card-verso">    
-                        <div class="atributos-atleta">
-                            <p class="nome"><strong>Nome:</strong> ${element.name}</p>
-                            <p class="genero"><strong>Gênero:</strong> ${element.gender}</p>
-                            <p class="esporte"><strong>Modalidade:</strong> ${element.sport}</p>
-                            <p class="pais"><strong>País:</strong> ${element.team}</p>
-                            <p class="medalha"><strong>Medalha:</strong> ${element.medal} ${medalicon}</p>
-                            <p class="evento"><strong>Categoria:</strong> ${element.event}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `
-  }).join("")
-  content.innerHTML = cards
-};
-*/
--- src/main.js
+++ src/main.js
@@ -1,37 +1,26 @@
-import { filterData } from './data.js';
-import athletes from './data/athletes/athletes.js';
+import { stringSport, filterSport, medalFilter } from './data.js';
 import data from './data/athletes/athletes.js';
 
 const content = document.querySelector('#cards-main'); // impressão dos cards
-const btnFilter = document.querySelector('#btn-filtro'); // botão de filtro
+//const btnFilter = document.querySelector('#btn-filtro'); // botão de filtro
 const medalSelector = document.querySelector('#medalhas'); // imputs de medalha
 const sportSelector = document.querySelector('#modalidades');
-const atletesSelector = document.querySelector('#atletas');
+const arrayAthletes = data.athletes;
 
-const arrayAthletes = data.athletes
-let arrayReturn = []
-
-
-
-sportSelector.onclick = (event) => {
-    arrayReturn = arrayAthletes.filter(medalFilter);
+function startPage() {
+    sportSelector.innerHTML = stringSport(arrayAthletes);
+    content.innerHTML = filterSport(arrayAthletes, sportSelector.value);
     console.log(sportSelector.value);
-    cardsPrint();
+    console.log('a página carregou');
 }
 
-btnFilter.onclick = (event) => {
-    arrayReturn = arrayAthletes.filter(medalFilter);
-    cardsPrint();
+startPage()
+
+sportSelector.onchange = (event) => {
+    content.innerHTML = filterSport(arrayAthletes, sportSelector.value);
+    console.log(sportSelector.value);
 }
 
-function medalFilter(atleta) {
-    if (medalSelector.value == 'All') {
-        return atleta.sport == sportSelector.value
-    }
-    return atleta.sport == sportSelector.value && atleta.medal == medalSelector.value;
-}
-
-// função para impressão dos cards
-function cardsPrint() {
-    content.innerHTML = filterData(arrayReturn);
+medalSelector.onchange = (event) => {
+    content.innerHTML = medalFilter(arrayAthletes, medalSelector.value, sportSelector.value);
 }
