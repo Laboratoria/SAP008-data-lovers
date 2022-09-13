c1a320c2f3f59ed872420575af8a126417fc57cf
Thainaprado7 <thaina.prado.psic@gmail.com>
2022-09-13 17:32:46 -0300

    print movies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import data from "./data/ghibli/ghibli.js"
 //* importei do arquivo ghibli.js para o main
+
+//*função printar cards personagens
 function getCharacters(data) {
     let lista_de_personagens = []
 
@@ -44,3 +46,39 @@ const lista_bruta = getCharacters(data);
 //* consolidado de dados para printar o card
 printCharacter(lista_bruta);
 //*chamando a função declarada em printcharacters.
+
+//*função printar cards filmes
+function getMovies(data) {
+    let lista_de_filmes = []
+
+    for (let contador = 0; contador < data.films.length; contador++) {
+        let filme_atual = data.films[contador];
+
+        for (let contador_movies = 0; contador_movies < filme_atual.length; contador_moveis++) {
+            lista_de_filmes.push({ "movie": filme_atual.title, ...filme_atual[contador_movies] })
+        }
+    }
+
+
+    return lista_de_filmes
+}
+
+function printMovies (data) {
+    const movies = document.getElementById ('movies');
+
+    for (let contador_print = 0; contador_print < data.length; contador_print++) {
+        let new_element = document.createElement("section")
+        new_element.innerHTML = `
+            <p>Filme: ${data[contador_print].title}</p><br>
+            <p>Img: <img src="${data[contador_print].poster}"/></p><br>
+            <p>Ano: ${data[contador_print].release_date}</p><br>
+            <p>Diretor: ${data[contador_print].director}</p><br>  
+                                 
+        `;
+        cards.appendChild(new_element);
+    
+    }
+}
+
+const lista_filmes = getMovies (data);
+printMovies(lista_filmes);
