14d79f0e9253a263e6b366be38b41e69af4c3cf5
adevcarol <alvscarolina@outlook.com>
2022-09-14 17:02:39 -0300

    Ajustes na função de filtro

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,11 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterTeam } from '../src/data.js';
 
-
-describe('example', () => {
+describe('filterTeam', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+    expect(typeof filterTeam).toBe('function');
+  })
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns `filterTeam`', () => {
+    expect(filterTeam()).toBe('filterTeam');
   });
 });
