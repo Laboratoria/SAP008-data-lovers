3d73356383a8593576e9a35242078943cdd669e0
Thainaprado7 <thaina.prado.psic@gmail.com>
2022-09-21 21:23:07 -0300

    js main

--- src/main.js
+++ src/main.js
@@ -3,23 +3,23 @@ import data from "./data/ghibli/ghibli.js"
 let filtros = document.querySelector('div#filtros');
 let printaCards = document.querySelector('section#printaCards')
 
-function cardTemplate(data) {
+function cardTemplate(obj) {
     return `
         <acticle>
-            <p>Nome: ${data.name}</p>
-            <p>Filme: ${data.movie}</p>
-            <p>image: <img src="${data.img}"/></p>
-            <p>Gênero: ${data.gender}</p>
-            <p>Idade: ${data.age}</p>
-            <p>Espécie: ${data.specie}</p>
+            <p>Nome: ${obj.name}</p>
+            <p>Filme: ${obj.movie}</p>
+            <p>image: <img src="${obj.img}"/></p>
+            <p>Gênero: ${obj.gender}</p>
+            <p>Idade: ${obj.age}</p>
+            <p>Espécie: ${obj.specie}</p>
         </acticle>
         `;
 }
 
 function montaCard(lista) {
     return lista
-        .map((data) => {
-            return cardTemplate(data);
+        .map((obj) => {
+            return cardTemplate(obj);
         })
 }
 
