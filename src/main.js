80bcc14c0777cb19be9e010143858cc89e2333fa
Thainaprado7 <thaina.prado.psic@gmail.com>
2022-09-22 01:14:49 -0300

    print card filmes js

--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
 import data from "./data/ghibli/ghibli.js"
 
-let filtros = document.querySelector('div#filtros');
 let printaCards = document.querySelector('section#printaCards')
 
-function cardTemplate(obj) {
+function PrintandoCard(filmsList){
     return `
-        <acticle>
-            <p>Nome: ${obj.name}</p>
-            <p>Filme: ${obj.movie}</p>
-            <p>image: <img src="${obj.img}"/></p>
-            <p>Gênero: ${obj.gender}</p>
-            <p>Idade: ${obj.age}</p>
-            <p>Espécie: ${obj.specie}</p>
-        </acticle>
+        <article>
+            <p>Title: ${filmsList.title}</p>
+            <img src="${filmsList.poster}"/></p>
+            <p>Director: ${filmsList.director}</p>
+            <p>Release date: ${filmsList.release_date}</p>
+        </article>
         `;
 }
 
 function montaCard(lista) {
     return lista
-        .map((obj) => {
-            return cardTemplate(obj);
+        .map((filmsList) => {
+            return PrintandoCard(filmsList);
         })
+        .join("");
 }
 
 printaCards.innerHTML = montaCard(data.films);
