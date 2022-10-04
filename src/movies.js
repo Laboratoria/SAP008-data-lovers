72b6e39a2a1436385a7b1134dabbc21fe0c09029
Thainaprado7 <thaina.prado.psic@gmail.com>
2022-10-03 21:02:29 -0300

    filtro director movies

--- src/movies.js
+++ src/movies.js
@@ -1,30 +1,63 @@
 import data from "./data/ghibli/ghibli.js"
 
-function extraindoListas(filmsList) {
-    let x = filmsList.map ((films) => films.release_date);
-    return new Set(x);
-}
-
-extraindoListas(data.films);
-
-function PrintandoCard(filmsList) {
+function PrintandoCard(film) {
     return `
         <article>
-            <p>Title: ${filmsList.title}</p>
-            <img id= "imagem1" src="${filmsList.poster}"/></p>
-            <p>Director: ${filmsList.director}</p>
-            <p>Release date: ${filmsList.release_date}</p>
+            <p>Title: ${film.title}</p>
+            <img id= "imagem1" src="${film.poster}"/></p>
+            <p>Director: ${film.director}</p>
+            <p>Release date: ${film.release_date}</p>
         </article>
         `;
 }
 
 function montaCard(lista) {
+    let directorFilter = document.getElementById("filterDirector").value;
+
+    if (directorFilter){
+        console.log(`O valor do direto é ${directorFilter}`);
+        lista =  lista.filter((obj) => obj.director == directorFilter);
+    }
+
     return lista
-        .map((filmsList) => {
-            return PrintandoCard(filmsList);
+        .map((film) => {
+            return PrintandoCard(film);
         })
         .join("");
 }
 
 printaCards.innerHTML = montaCard(data.films);
+
+// aqui termina a função de printar cards //
+
+// aqui começa a função de filtros //
+
+printaFiltroDirector(data.films);
+
+function printaFiltroDirector(filmsList){
+    let filterDirector = document.getElementById("filterDirector");
+    let directorList = filmsList.map((film) => film.director);
+    printaSelect(new Set(directorList), filterDirector);
+
+}
+
+function printaSelect(objList, campoDoFiltro){
+    console.log(campoDoFiltro)
+    objList.forEach((obj)=> {
+        var option = document.createElement("option");
+        option.text = obj;
+        option.value = obj;    
+        campoDoFiltro.appendChild(option);
+    })
+}
+
+const classe_filtros = document.querySelectorAll(".conteudo-principal-filtros");
+
+classe_filtros.forEach((item) => {
+    item.addEventListener("change", (e) => {
+        printaCards.innerHTML = "";
+        console.log("Limpando a listagem, antes de passar o filtro !")
+        printaCards.innerHTML = montaCard(data.films);
+    });
+})
 
