fb767804127282fe1376542331eb405f066ca6b1
raquelthabata <r.thabata05@gmail.com>
2022-10-07 18:13:30 -0300

    criação de filtro para pegar idade personagens

--- src/characters.js
+++ src/characters.js
@@ -1,9 +1,9 @@
 import data from "./data/ghibli/ghibli.js"
 const printCharacters = document.getElementById("printCharacters");
-function generateCharactersMovies(movies){
+function generateCharactersMovies(movies) {
     let layout = "";
-    movies.forEach( movie => {
-        movie.people.forEach( p => {
+    movies.forEach(movie => {
+        movie.people.forEach(p => {
             layout += `<div>
             <p> Name: ${p.name}</p>
             <img id= "imagem2" src = "${p.img}"/>
@@ -17,3 +17,43 @@ function generateCharactersMovies(movies){
 }
 
 generateCharactersMovies(data.films)
+
+printaFiltroAge(data.films);
+
+function printaFiltroAge(filmsList) {
+    const people = filmsList.map(movies => movies.people.filter((peopleAge) => peopleAge.age));
+
+    let arrIdade = [];
+    let newArr = [];
+
+    for (let i = 0; i < people.length; i++) {
+        let peo = people[i];
+        let res = peo.filter((e) => e.age);
+        res.forEach(b => arrIdade.push(b.age))
+
+    }
+    a
+    for (var i = 0; i < arrIdade.length; i++) {
+        if (newArr.indexOf(arrIdade[i]) == -1) {
+            newArr.push(arrIdade[i]);
+        }
+    }
+
+    let filterAge = document.getElementById("filterAge");
+    printaSelect(new Set(newArr), filterAge);
+
+
+}
+
+
+function printaSelect(objList, campoDoFiltro) {
+    objList.forEach((obj) => {
+        let option = document.createElement("option");
+        option.text = obj;
+        option.value = obj;
+        campoDoFiltro.appendChild(option);
+    })
+}
+
+
+
