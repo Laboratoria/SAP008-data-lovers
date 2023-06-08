039db7f8d8a62ba6c7e2a43bb9715d2026bf8f4f
deboramds <deborah.mart@hotmail.com>
2023-06-08 01:50:57 -0300

    implementando-função-slide-e-css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,27 @@ import { sortFilms, getCharacters, filterByCriteriaValue } from './data.js';
 
 import data from './data/ghibli/ghibli.js';
 
+//função slide automático
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+  nextImage();
+}, 2000)
+
+function nextImage(){
+  count++;
+  if(count>13){
+    count = 1;
+  }
+
+
+document.getElementById("radio"+count).checked = true;
+
+}
+
+//função printar filmes 
+
 const films = data.films
 
 function printFilmsCards(data) {
